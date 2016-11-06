3a066a3e85c8b06fc467394ebee7b07ea22285e4
hinojosa <hinojosa@in.tum.de>
2016-11-06 12:26:07 +0100

    Removed random fault generation from Manager

--- distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessManager.hpp
+++ distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessManager.hpp
@@ -251,27 +251,6 @@ CombiParameters& ProcessManager::getCombiParameters() {
   return params_;
 }
 
-
-/*
- * Create a certain given number of random faults, considering that the faulty processes
- * simply cannot give the evaluation results, but they are still available in the MPI
- * communication scheme (the nodes are not dead)
- */
-inline void
-ProcessManager::createRandomFaults( std::vector<int>& faultIds, int no_faults ) {
-  int fault_id;
-
-  // create random faults
-  int j = 0;
-  while (j < no_faults) {
-    fault_id = generate_random_fault( static_cast<int>( params_.getNumLevels() ) );
-    if (j == 0 || std::find(faultIds.begin(), faultIds.end(), fault_id) == faultIds.end()){
-      faultIds.push_back(fault_id);
-      j++;
-    }
-  }
-}
-
 /*
  * Recompute coefficients for the combination technique based on given grid faults using
  * an optimization scheme
