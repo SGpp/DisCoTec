99dba59bae3c2ec638c06d9530ab2fb60c684cf3
Alfredo Hinojosa <hinojosa@in.tum.de>
2017-03-01 11:21:30 +0100

    Added compile.sh file

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+USRNAME="ipvalf"
+scons -j 16 OPT=1 VERBOSE=1 SG_ALL=0 SG_DISTRIBUTEDCOMBIGRID=1 CXX=CC COMPILE_BOOST_TESTS=0 RUN_BOOST_TESTS=0 BOOST_LIBRARY_PATH=/zhome/academic/HLRS/ipv/$USRNAME/hlrs-tools/boost_1_58_0/stage/lib BOOST_INCLUDE_PATH=/zhome/academic/HLRS/ipv/$USRNAME/hlrs-tools/boost_1_58_0 GLPK_LIBRARY_PATH=/zhome/academic/HLRS/ipv/$USRNAME/hlrs-tools/glpk/lib GLPK_INCLUDE_PATH=/zhome/academic/HLRS/ipv/$USRNAME/hlrs-tools/glpk/include BUILD_STATICLIB=1
+
+scons -j 16 OPT=1 VERBOSE=1 SG_ALL=0 SG_DISTRIBUTEDCOMBIGRID=1 CXX=CC COMPILE_BOOST_TESTS=0 RUN_BOOST_TESTS=0 BOOST_LIBRARY_PATH=/zhome/academic/HLRS/ipv/$USRNAME/hlrs-tools/boost_1_58_0/stage/lib BOOST_INCLUDE_PATH=/zhome/academic/HLRS/ipv/$USRNAME/hlrs-tools/boost_1_58_0 GLPK_LIBRARY_PATH=/zhome/academic/HLRS/ipv/$USRNAME/hlrs-tools/glpk/lib GLPK_INCLUDE_PATH=/zhome/academic/HLRS/ipv/$USRNAME/hlrs-tools/glpk/include BUILD_STATICLIB=0 
