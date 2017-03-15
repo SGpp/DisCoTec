82d4a423c8c844bd4eaff77007a68b3d8611f1f6
Daniel Pfister <pfistedl@ipvs.uni-stuttgart.de>
2017-03-15 17:23:56 +0100

    Changed run.sh script for combi_example

--- distributedcombigrid/examples/combi_example/run.sh
+++ distributedcombigrid/examples/combi_example/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/path/to/SGpp/lib/sgpp:$LD_LIBRARY_PATH
-mpirun.mpich -n 5 ./combi_example
+
+NGROUP=$(grep ngroup ctparam | awk -F"=" '{print $2}')
+NPROCS=$(grep nprocs ctparam | awk -F"=" '{print $2}')
+
+mpirun.mpich -n $(($NGROUP*$NPROCS+1)) ./combi_example
