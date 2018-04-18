3056717b4f5a899e86aef76eed54f9aa7d0e1b57
Obersteiner <oberstei@in.tum.de>
2018-04-18 11:30:26 +0200

    fixed example for new CombiParameters interface

--- distributedcombigrid/examples/gene_distributed/src/manager.cpp
+++ distributedcombigrid/examples/gene_distributed/src/manager.cpp
@@ -284,7 +284,7 @@ int main(int argc, char** argv) {
     }
     // create combiparamters
     CombiParameters params( dim, lmin, lmax, boundary, levels,
-                            coeffs, hierarchizationDims, taskIDs, reduceCombinationDimsLmin, reduceCombinationDimsLmax, ncombi, numGrids);
+                            coeffs, hierarchizationDims, taskIDs, ncombi, reduceCombinationDimsLmin, reduceCombinationDimsLmax, numGrids);
     params.setParallelization(p);
 
     // create Manager with process groups
