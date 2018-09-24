17a482d17595b51235b2279881a17ac04bff1ded
Obersteiner <oberstei@in.tum.de>
2018-09-24 17:53:03 +0200

    added linear example to include path settings in adjust_examples.py

--- adjust_examples.py
+++ adjust_examples.py
@@ -2,7 +2,7 @@
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 GLPK_DIR= str(dir_path) + "/glpk"
-examples = ["combi_example", "combi_example_faults", "gene_distributed"]
+examples = ["combi_example", "combi_example_faults", "gene_distributed", "gene_distributed_linear"]
 for example in examples:
     pfilein = open(str(dir_path)+ "/distributedcombigrid/examples/" + example + "/Makefile.template" ,'r')
     temp = pfilein.read()
@@ -18,6 +18,14 @@ temp = pfilein.read()
 pfilein.close()
 temp = temp.replace('$(SGPP)', str(dir_path))
 pfileout = open(str(dir_path)+ "/distributedcombigrid/examples/gene_distributed/preproc.py" ,'w')
+pfileout.write(temp)
+pfileout.close()
+
+pfilein = open(str(dir_path)+ "/distributedcombigrid/examples/gene_distributed_linear/preproc.py" ,'r')
+temp = pfilein.read()
+pfilein.close()
+temp = temp.replace('$(SGPP)', str(dir_path))
+pfileout = open(str(dir_path)+ "/distributedcombigrid/examples/gene_distributed_linear/preproc.py" ,'w')
 pfileout.write(temp)
 pfileout.close()
 
