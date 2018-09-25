bd0bc1508aafaa892981d5880fb43b49e5cd2ec3
Obersteiner <oberstei@in.tum.de>
2018-09-25 14:39:17 +0200

    we now check if file exists before setting compilers

--- adjust_examples.py
+++ adjust_examples.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os 
 import sys
-
+import os.path
 CC = sys.argv[1]
 FC = sys.argv[2]
 cc = sys.argv[3]
@@ -24,18 +24,17 @@ for example in examples:
     pfileout.close()
 gene_versions = ["gene_mgr", "gene-non-linear"]
 for geneVersion in gene_versions:
-    pfilein = open(str(dir_path) + "/"+ geneVersion + "/makefiles/new_machine/new_machine.template" ,'r')
-    print(str(dir_path) + "/"+ geneVersion + "makefiles/new_machine/new_machine.template" )
-    temp = pfilein.read()
-    pfilein.close()
-    temp = temp.replace('$(SGPP)', str(dir_path))
-    temp = temp.replace('$(GLPK)', GLPK_DIR)
-    temp = temp.replace('$(CC)', CC)
-    temp = temp.replace('$(FC)', FC)
-    temp = temp.replace('$(cc)', cc)
-    pfileout = open(str(dir_path) + "/" +geneVersion + "/makefiles/new_machine/new_machine.mk" ,'w')
-    pfileout.write(temp)
-    pfileout.close()
+    fname = str(dir_path) + "/"+ geneVersion + "/makefiles/new_machine/new_machine"
+    if(os.path.isfile(fname + ".template")):
+        with open(fname + ".template",'r') as pfilein:
+            temp = pfilein.read()
+        temp = temp.replace('$(SGPP)', str(dir_path))
+        temp = temp.replace('$(GLPK)', GLPK_DIR)
+        temp = temp.replace('$(CC)', CC)
+        temp = temp.replace('$(FC)', FC)
+        temp = temp.replace('$(cc)', cc)
+        with open(fname + ".mk" ,'w') as pfileout:
+            pfileout.write(temp)
 pfilein = open(str(dir_path)+ "/distributedcombigrid/examples/gene_distributed/preproc.py" ,'r')
 temp = pfilein.read()
 pfilein.close()
