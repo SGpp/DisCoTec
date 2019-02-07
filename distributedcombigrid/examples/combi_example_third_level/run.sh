234394ea6a677222de16714ff71a7be554a81001
Marcel Hurler <mhurler123@googlemail.com>
2019-02-07 12:19:39 +0100

    sets SimpleAmqpClient library dir

--- distributedcombigrid/examples/combi_example_third_level/run.sh
+++ distributedcombigrid/examples/combi_example_third_level/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/path/to/SGpp/lib/sgpp:$LD_LIBRARY_PATH
+LIB_SGPP_DIR=../../../
+LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
 
+export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
+
+# TODO 
+#
 NGROUP=$(grep ngroup ctparam | awk -F"=" '{print $2}')
 NPROCS=$(grep nprocs ctparam | awk -F"=" '{print $2}')
 
-mpirun.mpich -n $(($NGROUP*$NPROCS+1)) ./combi_example
+mpich.mpirun -n $(($NGROUP*$NPROCS+1)) ./combi_example $1
