155fa168ed3963f0f8e28b3d54ef7798912bba7f
Marcel Hurler <mhurler123@googlemail.com>
2019-02-07 12:36:33 +0100

    using older version of TaskExample.hpp

--- distributedcombigrid/examples/combi_example_third_level/TaskExample.hpp
+++ distributedcombigrid/examples/combi_example_third_level/TaskExample.hpp
@@ -13,24 +13,20 @@
 
 namespace combigrid {
 
-class TaskExample : public Task {
+class TaskExample: public Task {
+
  public:
   /* if the constructor of the base task class is not sufficient we can provide an
    * own implementation. here, we add dt, nsteps, and p as a new parameters.
    */
-  TaskExample(DimType dim, LevelVector& l, std::vector<bool>& boundary, real coeff,
-              LoadModel* loadModel, real dt, size_t nsteps, IndexVector p = IndexVector(0),
-              FaultCriterion* faultCrit = (new StaticFaults({0, IndexVector(0), IndexVector(0)})))
-      : Task(dim, l, boundary, coeff, loadModel, faultCrit),
-        dt_(dt),
-        nsteps_(nsteps),
-        p_(p),
-        initialized_(false),
-        stepsTotal_(0),
-        dfg_(NULL) {}
+  TaskExample(DimType dim, LevelVector& l, std::vector<bool>& boundary,
+              real coeff, LoadModel* loadModel, real dt,
+              size_t nsteps, IndexVector p = IndexVector(0) ) :
+    Task(dim, l, boundary, coeff, loadModel), dt_(dt), nsteps_(
+      nsteps), p_(p), initialized_(false), stepsTotal_(0), dfg_(NULL) {
+  }
 
-  void init(CommunicatorType lcomm,
-            std::vector<IndexVector> decomposition = std::vector<IndexVector>()) {
+  void init(CommunicatorType lcomm) {
     assert(!initialized_);
     assert(dfg_ == NULL);
 
@@ -72,19 +68,21 @@ class TaskExample : public Task {
         p[dimMaxRatio] *= 2;
         prod_p = 1;
 
-        for (DimType k = 0; k < dim; ++k) prod_p *= p[k];
+        for (DimType k = 0; k < dim; ++k)
+          prod_p *= p[k];
       }
     } else {
       p = p_;
     }
 
     if (lrank == 0) {
-      std::cout << "init task " << this->getID() << " with l = " << this->getLevelVector()
-                << " and p = " << p << std::endl;
+      std::cout << "init task " << this->getID() << " with l = "
+                << this->getLevelVector() << " and p = " << p << std::endl;
     }
 
     // create local subgrid on each process
-    dfg_ = new DistributedFullGrid<CombiDataType>(dim, l, lcomm, this->getBoundary(), p);
+    dfg_ = new DistributedFullGrid<CombiDataType>(dim, l, lcomm,
+        this->getBoundary(), p);
 
     /* loop over local subgrid and set initial values */
     std::vector<CombiDataType>& elements = dfg_->getElementVector();
@@ -98,6 +96,7 @@ class TaskExample : public Task {
 
     initialized_ = true;
   }
+
 
   /* this is were the application code kicks in and all the magic happens.
    * do whatever you have to do, but make sure that your application uses
@@ -139,18 +138,22 @@ class TaskExample : public Task {
    * solution on one process and then converting it to the full grid representation.
    * the DistributedFullGrid class offers a convenient function to do this.
    */
-  void getFullGrid(FullGrid<CombiDataType>& fg, RankType r, CommunicatorType lcomm, int n = 0) {
+  void getFullGrid(FullGrid<CombiDataType>& fg, RankType r,
+                   CommunicatorType lcomm) {
     assert(fg.getLevels() == dfg_->getLevels());
 
     dfg_->gatherFullGrid(fg, r);
   }
 
-  DistributedFullGrid<CombiDataType>& getDistributedFullGrid(int n = 0) { return *dfg_; }
+  DistributedFullGrid<CombiDataType>& getDistributedFullGrid() {
+    return *dfg_;
+  }
 
   static real myfunction(std::vector<real>& coords, real t) {
     real u = std::cos(M_PI * t);
 
-    for (size_t d = 0; d < coords.size(); ++d) u *= std::cos(2.0 * M_PI * coords[d]);
+    for ( size_t d = 0; d < coords.size(); ++d )
+      u *= std::cos( 2.0 * M_PI * coords[d] );
 
     return u;
 
@@ -165,10 +168,8 @@ class TaskExample : public Task {
     */
   }
 
-  void setZero() {}
+  void setZero(){
 
-  ~TaskExample() {
-    if (dfg_ != NULL) delete dfg_;
   }
 
  protected:
@@ -177,7 +178,14 @@ class TaskExample : public Task {
    * this constructor before overwriting the variables that are set by the
    * manager. here we need to set the initialized variable to make sure it is
    * set to false. */
-  TaskExample() : initialized_(false), stepsTotal_(1), dfg_(NULL) {}
+  TaskExample() :
+    initialized_(false), stepsTotal_(1), dfg_(NULL) {
+  }
+
+  ~TaskExample() {
+    if (dfg_ != NULL)
+      delete dfg_;
+  }
 
  private:
   friend class boost::serialization::access;
@@ -200,7 +208,7 @@ class TaskExample : public Task {
    * For serialization of the parent class members, the class must be
    * registered with the BOOST_CLASS_EXPORT macro.
    */
-  template <class Archive>
+  template<class Archive>
   void serialize(Archive& ar, const unsigned int version) {
     // handles serialization of base class
     ar& boost::serialization::base_object<Task>(*this);
@@ -212,6 +220,6 @@ class TaskExample : public Task {
   }
 };
 
-}  // namespace combigrid
+} // namespace combigrid
 
 #endif /* TASKEXAMPLE_HPP_ */
