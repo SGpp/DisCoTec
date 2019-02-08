6370807b783d47aeefc855c00adff81a9ccd2ffb
Obersteiner <oberstei@in.tum.de>
2019-02-08 17:19:48 +0100

    added automatic updating of offsets

---
+++ distributedcombigrid/examples/gene_distributed/updateOffset.py
@@ -0,0 +1,16 @@
+from configparser import SafeConfigParser
+import collections
+# read parameter file
+parser = SafeConfigParser()
+parser.read('ctparam')
+
+config = collections.namedtuple('Config', 'checkpointFrequency ncombi')
+config.checkpointFrequency = int( parser.get('application','checkpointFrequency') )
+config.ncombi = int( parser.get('ct','ncombi') )
+
+with open('ginstance/offset.txt', 'r') as f:
+    offset = int(next(f).split()[1])
+with open('ginstance/offset.txt', 'w') as f:
+    new_offset_string = "offset " + str(int(offset + config.ncombi/config.checkpointFrequency)) + "\n"
+    f.write(new_offset_string) 
+
