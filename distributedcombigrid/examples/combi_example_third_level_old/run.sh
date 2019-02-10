cbb95e8d6641782a2d2c9ab3ed695285c1c275f1
Marcel Hurler <mhurler123@googlemail.com>
2019-02-10 16:44:18 +0100

    removed legacy third_level_old

--- distributedcombigrid/examples/combi_example_third_level_old/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=~/Git/combi/lib/sgpp:$LD_LIBRARY_PATH
-
-NGROUP=$(grep ngroup ctparam | awk -F"=" '{print $2}')
-NPROCS=$(grep nprocs ctparam | awk -F"=" '{print $2}')
-
-mpirun.mpich -n $(($NGROUP*$NPROCS+1)) ./combi_third_level $1
