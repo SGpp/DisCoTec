80f051bc8fa190072644f6a62c3c856d9f6354a4
Marcel Hurler <mhurler123@googlemail.com>
2019-02-12 10:20:40 +0100

    removes mpi path

--- distributedcombigrid/examples/combi_example_third_level/run.sh
+++ distributedcombigrid/examples/combi_example_third_level/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-MPI_DIR=/opt/mpich/bin/
 LIB_SGPP_DIR=../../../
 LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
 
@@ -10,4 +9,4 @@ export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_P
 NGROUP=$(grep ngroup ctparam | awk -F"=" '{print $2}')
 NPROCS=$(grep nprocs ctparam | awk -F"=" '{print $2}')
 
-$MPI_DIR/mpirun -n $(($NGROUP*$NPROCS+1)) ./combi_example $1
+mpirun -n $(($NGROUP*$NPROCS+1)) ./combi_example $1
