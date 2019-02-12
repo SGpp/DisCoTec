60b53fdc0bdaa07d3ced89de56b1a9e31e8722cb
Marcel Hurler <mhurler123@googlemail.com>
2019-02-12 10:16:57 +0100

    sets path to SimpleAmqp lib

--- distributedcombigrid/examples/combi_example_third_level/run.sh
+++ distributedcombigrid/examples/combi_example_third_level/run.sh
@@ -5,7 +5,7 @@ LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
 
 export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
 
-# TODO 
+# TODO
 #
 NGROUP=$(grep ngroup ctparam | awk -F"=" '{print $2}')
 NPROCS=$(grep nprocs ctparam | awk -F"=" '{print $2}')
