3ebe5744d80ad90b36c1669c7c8683bbf23b79d4
Marcel Hurler <mhurler123@googlemail.com>
2019-02-12 10:21:42 +0100

    adds path to SimpleAmqp lib

---
+++ distributedcombigrid/examples/combi_example_third_level/third_level_manager/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+LIB_SGPP_DIR=../../../../
+LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
+export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
+
+./thirdLevelManager
