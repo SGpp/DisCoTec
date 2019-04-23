a3a3a9400255823fe92dce19950e1df38649654f
Marcel Hurler <mhurler123@googlemail.com>
2019-04-23 10:22:34 +0200

    fixes test name

--- distributedcombigrid/tests/run_third_level_test.sh
+++ distributedcombigrid/tests/run_third_level_test.sh
@@ -4,4 +4,5 @@ LIB_SGPP_DIR=../../
 LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
 export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
 
-mpirun -n 9 -l ./test_distributedcombigrid_boost --run_test=managerSendRecv -- # additional params
+mpirun -n 3 -l xterm -hold -e gdb -ex run --args ./test_distributedcombigrid_boost --run_test=thirdLevel -- # additional params
+#mpirun -n 9 -l ./test_distributedcombigrid_boost --run_test=reduce -- # additional params
