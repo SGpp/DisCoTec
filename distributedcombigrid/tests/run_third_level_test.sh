5539031d9c9fbe5bc2d5d8417004e70fa715f780
Marcel Hurler <mhurler123@googlemail.com>
2019-05-06 10:15:27 +0200

    adds command for debugging

--- distributedcombigrid/tests/run_third_level_test.sh
+++ distributedcombigrid/tests/run_third_level_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+MPI_PATH=/opt/mpich/bin/
 LIB_SGPP_DIR=../../
 LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
 export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
 
-mpirun -n 3 -l xterm -hold -e gdb -ex run --args ./test_distributedcombigrid_boost --run_test=thirdLevel -- # additional params
-#mpirun -n 9 -l ./test_distributedcombigrid_boost --run_test=reduce -- # additional params
+#$MPI_PATH/mpirun -n 6 -l xterm -hold -e ./test_distributedcombigrid_boost --run_test=thirdLevel -- # additional params
+$MPI_PATH/mpirun -n 6 -l ./test_distributedcombigrid_boost --run_test=thirdLevel -- # additional params
