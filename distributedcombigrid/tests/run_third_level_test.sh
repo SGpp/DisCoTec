32d283a4b959a2a89896eb5aa25ee126f1dadaac
Marcel Hurler <mhurler123@googlemail.com>
2019-05-20 16:32:28 +0200

    adds calculation and passes correct number of procs

--- distributedcombigrid/tests/run_third_level_test.sh
+++ distributedcombigrid/tests/run_third_level_test.sh
@@ -5,5 +5,11 @@ LIB_SGPP_DIR=../../
 LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
 export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
 
-#$MPI_PATH/mpirun -n 6 -l xterm -hold -e ./test_distributedcombigrid_boost --run_test=thirdLevel -- # additional params
-$MPI_PATH/mpirun -n 6 -l ./test_distributedcombigrid_boost --run_test=thirdLevel -- # additional params
+ngroup=1
+nprocs=1
+total=$(( 2*(ngroup * nprocs + 1) ))
+
+# Use to simplify Debug:
+#$MPI_PATH/mpirun -n $total -l xterm -hold -e gdb -ex run --args ./test_distributedcombigrid_boost --run_test=thirdLevel -- $ngroup $nprocs # additional params
+
+$MPI_PATH/mpirun -n $total -l ./test_distributedcombigrid_boost --run_test=thirdLevel -- $ngroup $nprocs # additional params
