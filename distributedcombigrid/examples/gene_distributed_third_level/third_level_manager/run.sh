fd6ec2ceb1fe8b1c8843fcb82a45979362a29069
Marcel Hurler <mhurler123@googlemail.com>
2019-05-20 16:38:40 +0200

    prettification

--- distributedcombigrid/examples/gene_distributed_third_level/third_level_manager/run.sh
+++ distributedcombigrid/examples/gene_distributed_third_level/third_level_manager/run.sh
@@ -4,6 +4,4 @@ LIB_SGPP_DIR=/home/marci/UNI/HIWI/combi/
 LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
 export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
 
-MANAGER_DIR=$LIB_SGPP_DIR/distributedcombigrid/examples/gene_distributed_third_level/third_level_manager
-
-$MANAGER_DIR/thirdLevelManager $MANAGER_DIR/example.ini
+./thirdLevelManager example.ini
