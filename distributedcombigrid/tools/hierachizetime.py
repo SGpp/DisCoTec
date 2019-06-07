661866e08b23d091eadd6bb88fb8525dc7ad5187
Sven Hingst <sven.hingst@tum.de>
2019-06-07 09:44:56 +0200

    added simple hierachization time checking tool

---
+++ distributedcombigrid/tools/hierachizetime.py
@@ -0,0 +1,37 @@
+import json
+import sys
+import os
+import statistics
+
+
+if len(sys.argv) == 1:
+	print("using default file")
+	filename="distributedcombigrid/examples/combi_example/timers.json"
+else:
+	filename=sys.argv[1]
+
+data = json.load(open(filename))
+htimes=[]
+durations=[]
+dtimes=[]
+ddurations=[]
+for i in range(len(data)-1):
+	r = "rank" + str(i)
+	htimes.append(data[r]["events"]["combine hierarchize"])
+	durations.append([])
+	dtimes.append(data[r]["events"]["combine dehierarchize"])
+	ddurations.append([])
+
+for i in range(len(htimes)):
+	for j in range(len(htimes[i])):
+		durations[i].append(htimes[i][j][1]-htimes[i][j][0])
+	for j in range(len(dtimes[i])):
+		ddurations[i].append(dtimes[i][j][1]-dtimes[i][j][0])
+
+print("Hierachize")
+for i in range(len(htimes)):
+	print(i,": ", statistics.mean(durations[i])/1000,"ms\t",max(durations[i])/1000,"ms")
+
+print("Dehierachize")
+for i in range(len(dtimes)):
+	print(i,": ", statistics.mean(ddurations[i])/1000,"ms\t",max(ddurations[i])/1000,"ms")
