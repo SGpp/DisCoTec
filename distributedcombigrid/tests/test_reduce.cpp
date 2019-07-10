93d126e46481fa6ec60479fdf6b7174e806dde60
Keerthi Gaddameedi <keerthigaddameedi@gmail.com>
2019-07-10 15:48:00 +0200

    Update test_reduce to pass tests from issue 10

--- distributedcombigrid/tests/test_reduce.cpp
+++ distributedcombigrid/tests/test_reduce.cpp
@@ -196,8 +196,8 @@ void checkCombine(size_t ngroup = 1, size_t nprocs = 1) {
     // compare with known results:
     // point in the middle
     CombiDataType midResult = fg_eval.getData()[fg_eval.getNrElements() / 2];
-    std::cout << "midResult " << abs(midResult) << std::endl;
-    BOOST_TEST(abs(midResult) == 1.333333333);
+    std::cout << "midResult " << fabs(midResult) << std::endl;
+    BOOST_TEST(fabs(midResult) == 1.333333333);
 
     manager.exit();
   }
