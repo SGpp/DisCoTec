1e983bcf52fd47e036364371d7e03c308b2de14c
Marcel Hurler <mhurler123@googlemail.com>
2019-07-31 17:35:12 +0200

    now absolute paths, adds commands for debugging

--- distributedcombigrid/third_level_manager/run.sh
+++ distributedcombigrid/third_level_manager/run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-LIB_SGPP_DIR=/home/marci/UNI/HIWI/combi/
-LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
-export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
+SGPP_DIR=/home/marci/UNI/HIWI/combi/
+LIB_SIMPLE_AMQP_DIR=$SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
+export LD_LIBRARY_PATH=$SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
 
-./thirdLevelManager example.ini
+THIRD_LEVEL_MANAGER=$SGPP_DIR/distributedcombigrid/third_level_manager
+
+#while true; do
+#  $THIRD_LEVEL_MANAGER/thirdLevelManager $THIRD_LEVEL_MANAGER/example.ini
+#  sleep 3
+#done
+
+#gdb -ex run --args $THIRD_LEVEL_MANAGER/thirdLevelManager $THIRD_LEVEL_MANAGER/example.ini
+$THIRD_LEVEL_MANAGER/thirdLevelManager $THIRD_LEVEL_MANAGER/example.ini
