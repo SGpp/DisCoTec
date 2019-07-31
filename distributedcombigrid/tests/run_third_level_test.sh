a319ef65b0457c906c9423bcc12d4270939b6086
Marcel Hurler <mhurler123@googlemail.com>
2019-07-31 17:21:31 +0200

    Refactoring makes adding new test cases easier

--- distributedcombigrid/tests/run_third_level_test.sh
+++ distributedcombigrid/tests/run_third_level_test.sh
@@ -5,11 +5,13 @@ LIB_SGPP_DIR=../../
 LIB_SIMPLE_AMQP_DIR=$LIB_SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
 export LD_LIBRARY_PATH=$LIB_SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
 
-ngroup=2
-nprocs=2
-total=$(( 2*(ngroup * nprocs + 1) ))
+maxNgroup=2
+maxNprocs=2
+total=$(( 2*(maxNgroup * maxNprocs + 1) ))
+
+numCombinations=10
 
 # Use this to simplify Debug:
-#$MPI_PATH/mpirun -n $total -l xterm -hold -e gdb -ex run --args ./test_distributedcombigrid_boost --run_test=thirdLevel -- $ngroup $nprocs # additional params
+$MPI_PATH/mpirun -n $total -l xterm -hold -e gdb -ex run --args ./test_distributedcombigrid_boost --run_test=thirdLevel
 
-$MPI_PATH/mpirun -n $total -l ./test_distributedcombigrid_boost --run_test=thirdLevel -- $ngroup $nprocs # additional params
+#$MPI_PATH/mpirun -n $total -l ./test_distributedcombigrid_boost --run_test=thirdLevel -- # additional params
