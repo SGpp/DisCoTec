19802626f575ddbbb0d93e3ab959f5d3b0702fdc
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2019-09-04 13:38:06 +0200

    correct building and running for combi_example - TODO: fix error with decomposition!

--- distributedcombigrid/examples/combi_example/run.sh
+++ distributedcombigrid/examples/combi_example/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/path/to/SGpp/lib/sgpp:$LD_LIBRARY_PATH
+export PATH_TO_SGPP=`pwd`/../../../
+export LD_LIBRARY_PATH=$PATH_TO_SGPP/lib/sgpp:$LD_LIBRARY_PATH
 
 NGROUP=$(grep ngroup ctparam | awk -F"=" '{print $2}')
 NPROCS=$(grep nprocs ctparam | awk -F"=" '{print $2}')
 
-mpirun.mpich -n $(($NGROUP*$NPROCS+1)) ./combi_example
+mpirun.mpich -n $(($NGROUP*$NPROCS+1)) -l ./combi_example
