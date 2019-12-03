c53c80ace336caa429cf5b677a5f34b0b7619666
Marcel Hurler <mhurler123@googlemail.com>
2019-12-03 21:44:29 +0100

    Add id attribute to the Systems class which simplifys debug output

--- distributedcombigrid/third_level_manager/System.hpp
+++ distributedcombigrid/third_level_manager/System.hpp
@@ -6,13 +6,15 @@ class System
 {
   private:
     std::shared_ptr<ClientSocket> connection_;
+    size_t id_;
 
   public:
-    System(std::shared_ptr<ClientSocket>& connection);
+    System(std::shared_ptr<ClientSocket>& connection, size_t id);
 
     void sendMessage(const std::string& message) const;
     bool receiveMessage(std::string& message, int timeout=NetworkUtils::noTimeout) const;
     bool receivePosNumber(size_t& number, int timeout=NetworkUtils::noTimeout) const;
+    size_t getId() const;
 
     std::shared_ptr<ClientSocket> getConnection() const;
 };
