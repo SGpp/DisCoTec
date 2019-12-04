649609a967b2b2e5012c9fb34426513d1e5a2ad8
Marcel Hurler <mhurler123@googlemail.com>
2019-12-04 18:33:51 +0100

    removed legacy rabbitMQ

--- distributedcombigrid/third_level_manager/run.sh
+++ distributedcombigrid/third_level_manager/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 SGPP_DIR=/home/marci/UNI/HIWI/combi/
-LIB_SIMPLE_AMQP_DIR=$SGPP_DIR/distributedcombigrid/SimpleAmqpClient/build
-export LD_LIBRARY_PATH=$SGPP_DIR/lib/sgpp:$LIB_SIMPLE_AMQP_DIR:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$SGPP_DIR/lib/sgpp:$LD_LIBRARY_PATH
 
 THIRD_LEVEL_MANAGER=$SGPP_DIR/distributedcombigrid/third_level_manager
 
