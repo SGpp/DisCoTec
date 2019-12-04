a810ed137af7779af87f37bb505ca16d1acda75a
Marcel Hurler <mhurler123@googlemail.com>
2019-12-04 18:37:29 +0100

    Change SGPP to relative path

--- distributedcombigrid/third_level_manager/run.sh
+++ distributedcombigrid/third_level_manager/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SGPP_DIR=/home/marci/UNI/HIWI/combi/
+SGPP_DIR=../../
 export LD_LIBRARY_PATH=$SGPP_DIR/lib/sgpp:$LD_LIBRARY_PATH
 
 THIRD_LEVEL_MANAGER=$SGPP_DIR/distributedcombigrid/third_level_manager
