6eb0ecadbfbba4473314f8068f0d5acd9b96753a
Michael Obersteiner <oberstei@in.tum.de>
2019-12-05 15:59:13 +0100

    added legacy files again + added requirements to readme

---
+++ distributedcombigrid/src/sgpp/distributedcombigrid/legacy/CombiLinearBasisFunction.cpp
@@ -0,0 +1,4 @@
+#include <sgpp/distributedcombigrid/legacy/CombiLinearBasisFunction.hpp>
+
+const combigrid::BasisFunctionBasis* combigrid::LinearBasisFunction::defaultBasis_ =
+    new combigrid::LinearBasisFunction();
