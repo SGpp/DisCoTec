3246655e37f5212a9483d1f26f8b33b9897b30cf
Marvin Dostal <dostalmn@ipvs.uni-stuttgart.de>
2020-01-16 17:50:58 +0100

    Add documentation to load model interface

--- distributedcombigrid/src/sgpp/distributedcombigrid/loadmodel/LoadModel.hpp
+++ distributedcombigrid/src/sgpp/distributedcombigrid/loadmodel/LoadModel.hpp
@@ -6,9 +6,23 @@
 
 namespace combigrid {
 
+/**
+ * The LoadModel is an interface to be implemented to provide the expected load
+ * of specific tasks. It is to be used for scheduling the Tasks.
+ */
 class LoadModel {
  public:
   virtual ~LoadModel() = default;
+
+  /**
+   * Calculates the expected load of a task.
+   *
+   * The calculated load is in no specific unit. However, the 
+   * "greater than"-relation for two tasks has to be correct.
+   *
+   * @param l The level vector corresponding to a task.
+   * @returns Expected load of the given task.
+   */
   virtual real eval(const LevelVector& l) = 0;
 
 };
