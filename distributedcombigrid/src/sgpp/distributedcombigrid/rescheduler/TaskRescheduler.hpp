108ab4c728d69aadd8f263f34cad6a4de63e3a4b
Marvin Dostal <dostalmn@ipvs.uni-stuttgart.de>
2020-01-23 12:33:13 +0100

    Add task rescheduler interface

---
+++ distributedcombigrid/src/sgpp/distributedcombigrid/rescheduler/TaskRescheduler.hpp
@@ -0,0 +1,35 @@
+#ifndef TASKRESCHEDULER_HPP_
+#define TASKRESCHEDULER_HPP_
+
+#include <vector>
+#include <map>
+
+#include "sgpp/distributedcombigrid/utils/LevelVector.hpp"
+
+namespace combigrid {
+
+/**
+ * Interface of a task re-scheduler.
+ */
+class TaskRescheduler {
+ public:
+   /**
+    * Calculates changes in task distribution with the goal of a more balanced
+    * runtime of process groups.
+    *
+    * @param levelVectorToProcessGroupIndex The current task distribution.
+    * @param levelVectorToTaskDuration The last mesaured durations of the tasks 
+    *                                  run function.
+    * @returns Vector of pair with level vector and process group. To realize 
+    *          the calculated optimized task distribution; the task (defined by 
+    *          its level vector) has to be moved to the process group (defined 
+    *          by its process group id).
+    */
+  virtual std::vector<std::pair<LevelVector, int>> eval(
+      const std::map<LevelVector, int>& levelVectorToProcessGroupIndex,
+      const std::map<LevelVector, unsigned long>& levelVectorToTaskDuration) = 0;
+};
+
+} /* namespace combigrid */
+
+#endif /* TASKRESCHEDULER_HPP_ */
