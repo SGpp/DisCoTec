2cd0d47e328d79a3dd5355122ac7260b5f18bb94
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2020-12-02 10:55:07 +0100

    change tag numbering (to avoid error in travis?)
    (cherry picked from commit 8e613a3b2cd051f00d6e464bf2e2d834a4740ae7)

--- distributedcombigrid/src/sgpp/distributedcombigrid/mpi/MPITags.hpp
+++ distributedcombigrid/src/sgpp/distributedcombigrid/mpi/MPITags.hpp
@@ -5,15 +5,15 @@ namespace combigrid {
 // count down from maximum value, as lower tags indicate
 // subspaces in the dfg communication
 
-constexpr int TRANSFER_CLASS_TAG = std::numeric_limits<int>::max() - 1;
-constexpr int TRANSFER_DSGU_DATA_TAG = std::numeric_limits<int>::max() - 2;
-constexpr int TRANSFER_SUBSPACE_DATA_SIZES_TAG = std::numeric_limits<int>::max() - 3;
-constexpr int TRANSFER_TASK_TAG = std::numeric_limits<int>::max() - 4;
-constexpr int TRANSFER_LEVAL_TAG = std::numeric_limits<int>::max() - 5;
-constexpr int TRANSFER_SIGNAL_TAG = std::numeric_limits<int>::max() - 6;
-constexpr int TRANSFER_STATUS_TAG = std::numeric_limits<int>::max() - 7;
-constexpr int TRANSFER_TAG = std::numeric_limits<int>::max() - 8;
-constexpr int TRANSFER__TAG = std::numeric_limits<int>::max() - 9;
+constexpr int TRANSFER_CLASS_TAG = std::numeric_limits<int>::max() / 2 - 1;
+constexpr int TRANSFER_DSGU_DATA_TAG = std::numeric_limits<int>::max() / 2 - 2;
+constexpr int TRANSFER_SUBSPACE_DATA_SIZES_TAG = std::numeric_limits<int>::max() / 2 - 3;
+constexpr int TRANSFER_TASK_TAG = std::numeric_limits<int>::max() / 2 - 4;
+constexpr int TRANSFER_LEVAL_TAG = std::numeric_limits<int>::max() / 2 - 5;
+constexpr int TRANSFER_SIGNAL_TAG = std::numeric_limits<int>::max() / 2 - 6;
+constexpr int TRANSFER_STATUS_TAG = std::numeric_limits<int>::max() / 2 - 7;
+constexpr int TRANSFER_TAG = std::numeric_limits<int>::max() / 2 - 8;
+constexpr int TRANSFER__TAG = std::numeric_limits<int>::max() / 2 - 9;
 
 // hangup:
 // rescheduling/test_2: 3* `task size0` -> jetzt weiter
