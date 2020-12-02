1338f812651716389c1afed3f7ca7258c14329ca
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2020-12-02 11:10:00 +0100

    counting tags down without using <limits>
    (cherry picked from commit ac9a7e6d381d21fb6bc5f818aeb0299e9a6209fb)

--- distributedcombigrid/src/sgpp/distributedcombigrid/mpi/MPITags.hpp
+++ distributedcombigrid/src/sgpp/distributedcombigrid/mpi/MPITags.hpp
@@ -1,18 +1,19 @@
 #pragma once
-#include <limits>
 
 namespace combigrid {
 // count down from maximum value, as lower tags indicate
 // subspaces in the dfg communication
 
-constexpr int TRANSFER_CLASS_TAG = std::numeric_limits<int>::max() / 2 - 1;
-constexpr int TRANSFER_DSGU_DATA_TAG = std::numeric_limits<int>::max() / 2 - 2;
-constexpr int TRANSFER_SUBSPACE_DATA_SIZES_TAG = std::numeric_limits<int>::max() / 2 - 3;
-constexpr int TRANSFER_TASK_TAG = std::numeric_limits<int>::max() / 2 - 4;
-constexpr int TRANSFER_LEVAL_TAG = std::numeric_limits<int>::max() / 2 - 5;
-constexpr int TRANSFER_SIGNAL_TAG = std::numeric_limits<int>::max() / 2 - 6;
-constexpr int TRANSFER_STATUS_TAG = std::numeric_limits<int>::max() / 2 - 7;
-constexpr int TRANSFER_TAG = std::numeric_limits<int>::max() / 2 - 8;
-constexpr int TRANSFER__TAG = std::numeric_limits<int>::max() / 2 - 9;
+//constexpr int MAX_TAG =  MPI_TAG_UB; // avoided, because throws errors with OpenMPI
+constexpr int MAX_TAG = 32767; //from MPI Standard
+constexpr int TRANSFER_CLASS_TAG = MAX_TAG - 1;
+constexpr int TRANSFER_DSGU_DATA_TAG = MAX_TAG - 2;
+constexpr int TRANSFER_SUBSPACE_DATA_SIZES_TAG = MAX_TAG - 3;
+constexpr int TRANSFER_TASK_TAG = MAX_TAG - 4;
+constexpr int TRANSFER_LEVAL_TAG = MAX_TAG - 5;
+constexpr int TRANSFER_SIGNAL_TAG = MAX_TAG - 6;
+constexpr int TRANSFER_STATUS_TAG = MAX_TAG - 7;
+constexpr int TRANSFER_TAG = MAX_TAG - 8;
+constexpr int TRANSFER__TAG = MAX_TAG - 9;
 
 }  // namespace combigrid
