e12cf66123ee05e1a92c614d8ca60889908a4216
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2021-02-09 11:49:40 +0100

    selalib task: clean up some more; compilable

--- distributedcombigrid/examples/selalib_distributed/src/SelalibTask.hpp
+++ distributedcombigrid/examples/selalib_distributed/src/SelalibTask.hpp
@@ -46,23 +46,41 @@ class SelalibTask : public combigrid::Task {
   SelalibTask(DimType dim, LevelVector& l, std::vector<bool>& boundary, real coeff,
               LoadModel* loadModel, std::string& path, real dt, real combitime, size_t nsteps,
               IndexVector p = IndexVector(0),
-              FaultCriterion* faultCrit = (new StaticFaults({0, IndexVector(0), IndexVector(0)}))) {
-    assert("not yet implemented");
+              FaultCriterion* faultCrit = (new StaticFaults({0, IndexVector(0), IndexVector(0)})))
+      : path_(path),
+        p_(p),
+        localSize_(),
+        localDistribution_(nullptr),
+        dfg_(nullptr),
+        initialized_(false),
+        currentTime_(0.0),
+        currentTimestep_(0),
+        dt_(dt),
+        combitime_(combitime),
+        nsteps_(nsteps) {
+    assert(false && "not yet implemented");
   }
 
-  SelalibTask() { assert("not yet implemented"); }
+  SelalibTask()
+      : path_(),
+        p_(),
+        localSize_(),
+        localDistribution_(nullptr),
+        dfg_(nullptr),
+        initialized_(false),
+        currentTime_(0.0) {}
 
-  virtual ~SelalibTask() { assert("not yet implemented"); }
+  virtual ~SelalibTask() { assert(false && "not yet implemented"); }
   /**
    * lcomm is the local communicator of the process group.
    */
-  void run(CommunicatorType lcomm) { assert("not yet implemented"); }
+  void run(CommunicatorType lcomm) { assert(false && "not yet implemented"); }
 
   /**
    * This method changes the folder to the folder of the task
    * lcomm is the local communicator of the process group.
    */
-  void changeDir(CommunicatorType lcomm) { assert("not yet implemented"); }
+  void changeDir(CommunicatorType lcomm) { assert(false && "not yet implemented"); }
 
   /**
    * This method initializes the task
@@ -71,7 +89,7 @@ class SelalibTask : public combigrid::Task {
    */
   void init(CommunicatorType lcomm,
             std::vector<IndexVector> decomposition = std::vector<IndexVector>()) {
-    assert("not yet implemented");
+    assert(false && "not yet implemented");
   }
 
   /**
@@ -82,17 +100,27 @@ class SelalibTask : public combigrid::Task {
   /**
    * This method is used to decide if the execution of the task should fail
    */
-  void decideToKill() { assert("not yet implemented"); }
+  void decideToKill() { assert(false && "not yet implemented"); }
 
   /**
    * Returns the path of the task
    */
-  inline const std::string& getPath() const { assert("not yet implemented"); }
+  inline const std::string& getPath() const { assert(false && "not yet implemented"); }
 
   /**
    * This method writes the Selalib grid to the local checkpoint
    */
-  void writeLocalDistribution(double* data) { assert("not yet implemented"); }
+  void writeLocalDistribution(double* data) { assert(false && "not yet implemented"); }
+
+  /*
+   * Gather GENE checkpoint distributed over process group on process
+   * with localRootID and convert to FullGrid fg for speciefied species. The actual full grid
+   * will only be created on the process with localRootID.
+   */
+  void getFullGrid(FullGrid<CombiDataType>& fg, RankType lroot, CommunicatorType lcomm,
+                   int species) {
+    assert(false && "not yet implemented");
+  }
 
   /**
    * Returns the distributed full grid of the specified specie
@@ -100,23 +128,15 @@ class SelalibTask : public combigrid::Task {
   DistributedFullGrid<CombiDataType>& getDistributedFullGrid(int specie) { return *dfg_; }
 
   /**
-   * sets the dfg content to zero
+   * @return double* the pointer to the distribution (in Fortran allocated memory)
    */
-  void setZero() { assert("not yet implemented"); }
+  inline double* getLocalDistribution() { return localDistribution_; }
 
   /**
-   * Sets the total number of timesteps computed so far. Used in case of restart of component grids
-   * during fault recovery. Only valid if combitime is not used
+   * sets the dfg content to zero
    */
-  inline void setStepsTotal(size_t stepsTotal) { stepsTotal_ = stepsTotal; }
-  /**
-   * Sets the current combination step
-   */
-  inline void setCombiStep(int ncombi) { combiStep_ = ncombi; }
-  /**
-   * Returns the current combination step
-   */
-  inline int getCombiStep() { return combiStep_; }
+  void setZero() { assert(false && "not yet implemented"); }
+
   /**
    * Return boolean to indicate whether SelalibTask is initialized.
    */
@@ -164,7 +184,6 @@ class SelalibTask : public combigrid::Task {
   double* localDistribution_;
   DistributedFullGrid<CombiDataType>* dfg_;
 
-
   bool initialized_;  // indicates if SelalibTask is initialized
 
   /*
@@ -174,13 +193,11 @@ class SelalibTask : public combigrid::Task {
   real currentTimestep_;  // curent time step length in the simulation
   real dt_;
   real combitime_;  // simulation time interval between combinations
+
   /**number of time-steps in between two combinations (is set very large in case combitime should be
    * used); this requires equal time-steps for every component grid
    */
   size_t nsteps_;
-  size_t stepsTotal_;  // number of time-steps done so far (there might be multiple timesteps in
-                       // between two combinations)
-  size_t combiStep_;   // number of combinations done so far
 
   // std::chrono::high_resolution_clock::time_point  startTimeIteration_;
 
@@ -192,15 +209,11 @@ class SelalibTask : public combigrid::Task {
     ar& dt_;
     ar& combitime_;
     ar& nsteps_;
-    ar& stepsTotal_;
-    ar& combiStep_;
     // ar& localDistribution_;
     ar& p_;
     ar& currentTime_;
   }
 };
-
-inline const std::string& SelalibTask::getPath() const { return path_; }
 
 inline std::ostream& operator<<(std::ostream& os, const SelalibTask& t) {
   os << "SelalibTask:\n"
@@ -209,12 +222,6 @@ inline std::ostream& operator<<(std::ostream& os, const SelalibTask& t) {
 
   return os;
 }
-
-inline void SelalibTask::setStepsTotal(size_t stepsTotal) { stepsTotal_ = stepsTotal; }
-
-inline double* SelalibTask::getLocalDistribution() { return localDistribution_; }
-
-inline void SelalibTask::setCombiStep(int ncombi) { combiStep_ = ncombi; }
 
 } /* namespace combigrid */
 
