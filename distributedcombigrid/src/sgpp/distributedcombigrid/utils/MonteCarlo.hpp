d719b643b726bd94e5c6ec678461a94814e65062
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2021-03-19 17:15:19 +0100

    mc: have 1 seed, for reproducible tests

--- distributedcombigrid/src/sgpp/distributedcombigrid/utils/MonteCarlo.hpp
+++ distributedcombigrid/src/sgpp/distributedcombigrid/utils/MonteCarlo.hpp
@@ -5,13 +5,15 @@
 // #include <iterator>
 #include <functional>
 
+#include "sgpp/distributedcombigrid/utils/Config.hpp"
+
 namespace combigrid {
 namespace montecarlo {
 std::vector<std::vector<real>> getRandomCoordinates(int numCoordinates, size_t dim) {
   std::vector<std::vector<real>> randomCoords (numCoordinates, std::vector<real>(dim));
   // cf. https://stackoverflow.com/a/23143753
-  std::random_device rnd_device;
-  std::mt19937 mersenne_engine {rnd_device()};  // Generates random numbers
+  // std::random_device rnd_device;
+  std::mt19937 mersenne_engine {8285545262};  // have 1 seed, for reproducible tests
   std::uniform_real_distribution<> dist {0., 1.};
   auto gen = [&dist, &mersenne_engine](){
                   return dist(mersenne_engine);
