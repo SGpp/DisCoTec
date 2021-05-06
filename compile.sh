7359e45be3d809ce0432b40ab47f8b01d243a43b
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2021-05-06 09:36:34 +0200

    setting LD_LIBRARY_PATH for single tests

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/glpk/lib
 scons -j 8 ISGENE=0 VERBOSE=1 COMPILE_BOOST_TESTS=1 RUN_BOOST_TESTS=0 RUN_CPPLINT=0 BUILD_STATICLIB=0 CC=mpicc.mpich FC=mpifort.mpich CXX=mpicxx.mpich OPT=1 TIMING=0 UNIFORMDECOMPOSITION=1 ENABLEFT=0 #DEBUG_OUTPUT=1
 
+export LD_LIBRARY_PATH=$(pwd)/lib/sgpp:$(pwd)/glpk/lib:$LD_LIBRARY_PATH
 mpiexec.mpich -np 9 ./distributedcombigrid/tests/test_distributedcombigrid_boost --run_test=ftolerance
 mpiexec.mpich -np 9 ./distributedcombigrid/tests/test_distributedcombigrid_boost --run_test=fullgrid
 mpiexec.mpich -np 9 ./distributedcombigrid/tests/test_distributedcombigrid_boost --run_test=hierarchization
