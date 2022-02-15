045362de285b1491cf06eee5c7c4c5cbc8b66f07
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2022-02-15 07:22:55 +0100

    integration t: writeSparseGridMinMaxCoefficients

--- distributedcombigrid/tests/test_integration.cpp
+++ distributedcombigrid/tests/test_integration.cpp
@@ -151,6 +151,7 @@ void checkIntegration(size_t ngroup = 1, size_t nprocs = 1, bool boundaryV = tru
     BOOST_TEST_CHECKPOINT("write solution " + filename);
     Stats::startEvent("manager write solution");
     manager.parallelEval( lmax, filename, 0 );
+    manager.writeSparseGridMinMaxCoefficients("integration_" + std::to_string(boundaryV) + "_sparse_minmax");
     Stats::stopEvent("manager write solution");
     std::cout << "wrote solution  " << ngroup << " " << nprocs << std::endl;
 
