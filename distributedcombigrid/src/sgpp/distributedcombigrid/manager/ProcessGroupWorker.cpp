SGpp/DisCoTec

commit bea6109c5695da9c6c343e67194ea573fba98d5e

worker for third level: remove stats for individual 'run's and combine substeps

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessGroupWorker.cpp
@@ -120,7 +120,7 @@ SignalType ProcessGroupWorker::wait() {
         processDuration(*currentTask_, e, theMPISystem()->getNumProcs());
 
         if (!isGENE) {
-          Stats::stopEvent("worker run");
+          //Stats::stopEvent("worker run");
         }
       } else {
         std::cout << "Possible error: No tasks! \n";
@@ -400,7 +400,7 @@ void ProcessGroupWorker::ready() {
         currentTask_ = tasks_[i];
         // if isGENE, this is done in GENE's worker_routines.cpp
         if (!isGENE) {
-          Stats::startEvent("worker run");
+          //Stats::startEvent("worker run");
         }
         currentTask_->run(theMPISystem()->getLocalComm());
         Stats::Event e;
@@ -629,29 +629,29 @@ void ProcessGroupWorker::combineLocalAndGlobal() {
 #ifdef DEBUG_OUTPUT
   MASTER_EXCLUSIVE_SECTION { std::cout << "start combining \n"; }
 #endif
-  Stats::startEvent("combine zeroDsgsData");
+  //Stats::startEvent("combine zeroDsgsData");
   zeroDsgsData();
-  Stats::stopEvent("combine zeroDsgsData");
+  //Stats::stopEvent("combine zeroDsgsData");
 
-  Stats::startEvent("combine hierarchize");
+  //Stats::startEvent("combine hierarchize");
   hierarchizeFullGrids();
-  Stats::stopEvent("combine hierarchize");
+  //Stats::stopEvent("combine hierarchize");
 
 #ifdef DEBUG_OUTPUT
   MASTER_EXCLUSIVE_SECTION { std::cout << "mid combining \n"; }
 #endif
 
-  Stats::startEvent("combine local reduce");
+  //Stats::startEvent("combine local reduce");
   addFullGridsToUniformSG();
-  Stats::stopEvent("combine local reduce");
+  //Stats::stopEvent("combine local reduce");
 
 #ifdef DEBUG_OUTPUT
   MASTER_EXCLUSIVE_SECTION { std::cout << "almost done combining \n"; }
 #endif
 
-  Stats::startEvent("combine global reduce");
+  //Stats::startEvent("combine global reduce");
   reduceUniformSG();
-  Stats::stopEvent("combine global reduce");
+  //Stats::stopEvent("combine global reduce");
 
 #ifdef DEBUG_OUTPUT
   MASTER_EXCLUSIVE_SECTION { std::cout << "end combining \n"; }

commit 6c50a3ac8cb50b576d6fb90abeb96872ec529732

worker : fix stats error introduced in bea6109c56

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessGroupWorker.cpp
@@ -119,9 +119,6 @@ SignalType ProcessGroupWorker::wait() {
         // std::cout << "from runnext ";
         processDuration(*currentTask_, e, theMPISystem()->getNumProcs());
 
-        if (!isGENE) {
-          //Stats::stopEvent("worker run");
-        }
       } else {
         std::cout << "Possible error: No tasks! \n";
       }
@@ -376,6 +373,9 @@ SignalType ProcessGroupWorker::wait() {
       currentTask_ = tasks_.back();
     }
   }
+  if (!isGENE && signal == RUN_NEXT) {
+    Stats::stopEvent("worker run");
+  }
   return signal;
 }
 
@@ -409,14 +409,14 @@ void ProcessGroupWorker::ready() {
         // set currentTask
         currentTask_ = tasks_[i];
         // if isGENE, this is done in GENE's worker_routines.cpp
-        if (!isGENE) {
-          //Stats::startEvent("worker run");
-        }
+        // if (!isGENE) {
+        //   Stats::startEvent("worker run");
+        // }
         currentTask_->run(theMPISystem()->getLocalComm());
         Stats::Event e;
-        if (!isGENE) {
-          Stats::stopEvent("worker run");
-        }
+        // if (!isGENE) {
+        //   Stats::stopEvent("worker run");
+        // }
 
         processDuration(*currentTask_, e, theMPISystem()->getNumProcs());
         if (ENABLE_FT) {