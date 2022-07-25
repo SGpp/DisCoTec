0638707047d3beca85357102a6a5b6a6cd79b218
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2022-07-25 14:38:25 +0200

    MonteCarlo: add getNumberSequenceFromSeed function
    (cherry picked from commit ba1aa72623d4065ec95e6c4dbc3872f2ec9f0336)

--- distributedcombigrid/src/sgpp/distributedcombigrid/utils/MonteCarlo.hpp
+++ distributedcombigrid/src/sgpp/distributedcombigrid/utils/MonteCarlo.hpp
@@ -4,6 +4,7 @@
 namespace combigrid {
 namespace montecarlo {
 std::vector<std::vector<real>> getRandomCoordinates(int numCoordinates, size_t dim);
+void getNumberSequenceFromSeed(std::vector<real>& randomNumsToBeSet, size_t seed);
 real getRandomNumber(real&& a, real&& b);
 }
 }
