SGpp/DisCoTec

commit 566b85a461c94fd40120ee7d855489b6d47dc711

new feature: third level combination w/ smaller extra sparse grid (activate with extraSparseGrid = 1)

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessGroupWorker.cpp
@@ -214,9 +214,15 @@ SignalType ProcessGroupWorker::wait() {
       Stats::stopEvent("waitForThirdLevelCombiResult");
 
     } break;
+    case REDUCE_SUBSPACE_SIZES_TL_AND_ALLOCATE_EXTRA_SG: {
+      Stats::startEvent("unifySubspaceSizesThirdLevelExtra");
+      reduceSubspaceSizesThirdLevel(true);
+      Stats::stopEvent("unifySubspaceSizesThirdLevelExtra");
+
+    } break;
     case REDUCE_SUBSPACE_SIZES_TL: {
       Stats::startEvent("unifySubspaceSizesThirdLevel");
-      reduceSubspaceSizesThirdLevel();
+      reduceSubspaceSizesThirdLevel(false);
       Stats::stopEvent("unifySubspaceSizesThirdLevel");
 
     } break;
@@ -1196,23 +1202,45 @@ void ProcessGroupWorker::combineThirdLevel() {
   const RankType& manager = theMPISystem()->getThirdLevelManagerRank();
 
   std::vector<MPI_Request> requests;
-  for (auto& dsg : combinedUniDSGVector_) {
-    assert(dsg->getRawDataSize() < 2147483647 && "Dsg is larger than 2^31-1 and can not be "
+  for (size_t i = 0; i < combinedUniDSGVector_.size(); ++i) {
+    auto uniDsg = combinedUniDSGVector_[i].get();
+    auto dsgToUse = uniDsg;
+    if (extraUniDSGVector_.size() > 0) {
+      dsgToUse = extraUniDSGVector_[i].get();
+    }
+    assert(dsgToUse->getRawDataSize() < 2147483647 && "Dsg is larger than 2^31-1 and can not be "
                                              "sent in a single MPI Call (not "
                                              "supported yet) try a more coarse"
                                              "decomposition");
+    // if we have an extra dsg for third level exchange, we use it
+    if (extraUniDSGVector_.size() > 0) {
+      // copy partial data from uniDSG to extraDSG
+      for (size_t i = 0; i < uniDsg->getNumSubspaces(); ++i) {
+        assert(dsgToUse->getDataSize(i) == 0 ||
+               dsgToUse->getDataSize(i) == uniDsg->getDataSize()[i]);
+        std::copy_n(uniDsg->getData(i), dsgToUse->getDataSize(i), dsgToUse->getData(i));
+      }
+    }
+
     // send dsg data to manager
     Stats::startEvent("combine send dsg data to manager");
-    sendDsgData(dsg.get(), manager, managerComm);
+    sendDsgData(dsgToUse, manager, managerComm);
     Stats::stopEvent("combine send dsg data to manager");
 
     // recv combined dsgu from manager
     Stats::startEvent("combine recv combined data from manager");
-    recvDsgData(dsg.get(), manager, managerComm);
+    recvDsgData(dsgToUse, manager, managerComm);
     Stats::stopEvent("combine recv combined data from manager");
 
+    if (extraUniDSGVector_.size() > 0) {
+      // copy partial data from extraDSG back to uniDSG
+      for (size_t i = 0; i < uniDsg->getNumSubspaces(); ++i) {
+        std::copy_n(dsgToUse->getData(i), dsgToUse->getDataSize(i), uniDsg->getData(i));
+      }
+    }
+
     // distribute solution in globalReduceComm to other pgs
-    MPI_Request request = asyncBcastDsgData(dsg.get(), globalReduceRank, globalReduceComm);
+    MPI_Request request = asyncBcastDsgData(uniDsg, globalReduceRank, globalReduceComm);
     requests.push_back(request);
   }
   // update fgs
@@ -1227,7 +1255,7 @@ void ProcessGroupWorker::combineThirdLevel() {
 
 /** Reduces subspace sizes with remote.
  */
-void ProcessGroupWorker::reduceSubspaceSizesThirdLevel() {
+void ProcessGroupWorker::reduceSubspaceSizesThirdLevel(bool thirdLevelExtraSparseGrid) {
   assert(combiParametersSet_);
 
   // prepare a buffer with enough space for all subspace sizes from all dsgs
@@ -1258,21 +1286,41 @@ void ProcessGroupWorker::reduceSubspaceSizesThirdLevel() {
   MPI_Gatherv(buff.data(), bufSize, dtype, nullptr, nullptr,
               nullptr, dtype, thirdLevelManagerRank, thirdLevelComm);
 
+  if (thirdLevelExtraSparseGrid && extraUniDSGVector_.empty()) {
+    // create new vector for extra sparse grids (that will be only on this process group)
+    extraUniDSGVector_.resize(combinedUniDSGVector_.size());
+    for (auto& extraUniDSG : extraUniDSGVector_) {
+      extraUniDSG = std::unique_ptr<DistributedSparseGridUniform<CombiDataType>>(
+          new DistributedSparseGridUniform<CombiDataType>(
+              combinedUniDSGVector_[0]->getDim(), combinedUniDSGVector_[0]->getAllLevelVectors(),
+              combinedUniDSGVector_[0]->getBoundaryVector(), theMPISystem()->getLocalComm()));
+    }
+  }
+
   // receive updated sizes from manager
   MPI_Scatterv(nullptr, 0, nullptr, dtype,
                buff.data(), bufSize, dtype, thirdLevelManagerRank, thirdLevelComm);
 
-  // distribute updated sizes to workers with same decomposition (global reduce comm)
-  CommunicatorType globalReduceComm = theMPISystem()->getGlobalReduceComm();
-  RankType globalReduceRank = theMPISystem()->getGlobalReduceRank();
-  MPI_Bcast(buff.data(), bufSize, dtype, globalReduceRank, globalReduceComm);
+  if (!thirdLevelExtraSparseGrid) {
+    // distribute updated sizes to workers with same decomposition (global reduce comm)
+    // cf. waitForThirdLevelSizeUpdate(), which is called in other process groups
+    CommunicatorType globalReduceComm = theMPISystem()->getGlobalReduceComm();
+    RankType globalReduceRank = theMPISystem()->getGlobalReduceRank();
+    MPI_Bcast(buff.data(), bufSize, dtype, globalReduceRank, globalReduceComm);
+  }
 
+  // update either old or new sparse grids
+  auto* uniDSGVectorToSet = &combinedUniDSGVector_;
+  if (thirdLevelExtraSparseGrid) {
+    uniDSGVectorToSet = &extraUniDSGVector_;
+  }
   // set updated sizes in dsgs
   std::vector<size_t>::iterator buffIt = buff.begin();
-  for (auto& dsg : combinedUniDSGVector_) {
+  for (auto& dsg : *uniDSGVectorToSet) {
     for(size_t i = 0; i < dsg->getNumSubspaces(); ++i) {
       dsg->setDataSize(i, *(buffIt++));
     }
+    dsg->createSubspaceData();
 #ifdef DEBUG_OUTPUT
     MASTER_EXCLUSIVE_SECTION {
       std::cout << "third level reduce dsg size: " << dsg->getRawDataSize() << " * "

distributedcombigrid/examples/distributed_third_level/combi_example.cpp
@@ -162,6 +162,7 @@ int main(int argc, char** argv) {
   unsigned int systemNumber = 0, numSystems = 1;
   unsigned short thirdLevelPort = 0;
   bool hasThirdLevel = static_cast<bool>(cfg.get_child_optional("thirdLevel"));
+  bool extraSparseGrid = true;
   std::vector<real> fractionsOfScheme;
   if (hasThirdLevel) {
     std::cout << "Using third-level parallelism" << std::endl;
@@ -170,6 +171,7 @@ int main(int argc, char** argv) {
     numSystems = cfg.get<unsigned int>("thirdLevel.numSystems");
     thirdLevelPort = cfg.get<unsigned short>("thirdLevel.port");
     thirdLevelSSHCommand = cfg.get<std::string>("thirdLevel.sshCommand", "");
+    extraSparseGrid = cfg.get<bool>("thirdLevel.extraSparseGrid");
     bool hasFractions = static_cast<bool>(cfg.get_child_optional("thirdLevel.fractionsOfScheme"));
     if (hasFractions) {
       std::string fractionsString = cfg.get<std::string>("thirdLevel.fractionsOfScheme");
@@ -381,8 +383,8 @@ int main(int argc, char** argv) {
     // exchange subspace sizes to unify the dsgs in the third level case
     if (hasThirdLevel) {
       Stats::startEvent("manager unify subspace sizes with remote");
-      manager.unifySubspaceSizesThirdLevel(),
-          Stats::stopEvent("manager unify subspace sizes with remote");
+      manager.unifySubspaceSizesThirdLevel(extraSparseGrid);
+      Stats::stopEvent("manager unify subspace sizes with remote");
     }
 
     double start, finish;

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessGroupManager.cpp
@@ -128,9 +128,14 @@ bool ProcessGroupManager::combineThirdLevel(const ThirdLevelUtils& thirdLevel,
  */
 bool ProcessGroupManager::reduceLocalAndRemoteSubspaceSizes(const ThirdLevelUtils& thirdLevel,
                                                             CombiParameters& params,
-                                                            bool isSendingFirst) {
+                                                            bool isSendingFirst,
+                                                            bool thirdLevelExtraSparseGrid) {
   // tell workers to perform reduce
-  sendSignalAndReceive(REDUCE_SUBSPACE_SIZES_TL);
+  if (thirdLevelExtraSparseGrid) {
+    sendSignalAndReceive(REDUCE_SUBSPACE_SIZES_TL_AND_ALLOCATE_EXTRA_SG);
+  } else {
+    sendSignalAndReceive(REDUCE_SUBSPACE_SIZES_TL);
+  }
 
   // prepare buffers
   std::unique_ptr<uint64_t[]> sendBuff;
@@ -155,13 +160,32 @@ bool ProcessGroupManager::reduceLocalAndRemoteSubspaceSizes(const ThirdLevelUtil
     // send subspace sizes to remote
     thirdLevel.sendData(sendBuff.get(), buffSize);
   }
+  
+  // set accumulated dsgu sizes per worker
+  formerDsguDataSizePerWorker_.resize(numSubspacesPerWorker.size());
+  uint64_t* sizePtr = sendBuff.get();
+  for (size_t w = 0; w < numSubspacesPerWorker.size(); ++w) {
+    int sum = 0;
+    for (int ss = 0; ss < numSubspacesPerWorker[w]; ++ss) {
+      sum += (int)*(sizePtr++);
+    }
+    formerDsguDataSizePerWorker_[w] = sum;
+  }
 
-  // perform max reduce
-  for (size_t i = 0; i < buffSize; ++i) sendBuff[i] = std::max(sendBuff[i], recvBuff[i]);
+  if (thirdLevelExtraSparseGrid) {
+    // perform min reduce
+    for (size_t i = 0; i < buffSize; ++i) sendBuff[i] = std::min(sendBuff[i], recvBuff[i]);
+  } else {
+    // perform max reduce
+    for (size_t i = 0; i < buffSize; ++i) sendBuff[i] = std::max(sendBuff[i], recvBuff[i]);
+  }
+
+  // scatter data back to workers
+  distributeSubspaceSizes(thirdLevel, params, sendBuff, buffSize, numSubspacesPerWorker);
 
   // set accumulated dsgu sizes per worker
   dsguDataSizePerWorker_.resize(numSubspacesPerWorker.size());
-  uint64_t* sizePtr = sendBuff.get();
+  sizePtr = sendBuff.get();
   for (size_t w = 0; w < numSubspacesPerWorker.size(); ++w) {
     int sum = 0;
     for (int ss = 0; ss < numSubspacesPerWorker[w]; ++ss) {
@@ -169,9 +193,6 @@ bool ProcessGroupManager::reduceLocalAndRemoteSubspaceSizes(const ThirdLevelUtil
     }
     dsguDataSizePerWorker_[w] = sum;
   }
-
-  // scatter data back to workers
-  distributeSubspaceSizes(thirdLevel, params, sendBuff, buffSize, numSubspacesPerWorker);
   return true;
 }
 

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessGroupManager.hpp
@@ -145,6 +145,7 @@ class ProcessGroupManager {
   simft::Sim_FT_MPI_Request statusRequestFT_;
 
   // stores the accumulated dsgu sizes per worker
+  std::vector<size_t> formerDsguDataSizePerWorker_;
   std::vector<size_t> dsguDataSizePerWorker_;
 
   void recvStatus();
@@ -162,25 +163,23 @@ class ProcessGroupManager {
 
   inline void setProcessGroupBusyAndReceive();
 
-  void exchangeDsgus(const ThirdLevelUtils& thirdLevel,
-                           CombiParameters& params,
-                           bool isSendingFirst);
+  void exchangeDsgus(const ThirdLevelUtils& thirdLevel, CombiParameters& params,
+                     bool isSendingFirst);
 
-  bool collectSubspaceSizes(const ThirdLevelUtils& thirdLevel,
-                            CombiParameters& params,
-                            std::unique_ptr<uint64_t[]>& buff,
-                            size_t& buffSize,
+  bool collectSubspaceSizes(const ThirdLevelUtils& thirdLevel, CombiParameters& params,
+                            std::unique_ptr<uint64_t[]>& buff, size_t& buffSize,
                             std::vector<int>& numSubspacesPerWorker);
 
-  bool distributeSubspaceSizes(const ThirdLevelUtils& thirdLevel,
-                               CombiParameters& params,
-                               const std::unique_ptr<uint64_t[]>& buff,
-                               size_t buffSize,
+  bool distributeSubspaceSizes(const ThirdLevelUtils& thirdLevel, CombiParameters& params,
+                               const std::unique_ptr<uint64_t[]>& buff, size_t buffSize,
                                const std::vector<int>& numSubspacesPerWorker);
 
-  bool reduceLocalAndRemoteSubspaceSizes(const ThirdLevelUtils& thirdLevel,
-                                         CombiParameters& params,
-                                         bool isSendingFirst);
+  bool reduceLocalAndRemoteSubspaceSizes(const ThirdLevelUtils& thirdLevel, CombiParameters& params,
+                                         bool isSendingFirst, bool thirdLevelExtraSparseGrid);
+
+  const std::vector<size_t>& getFormerDsguDataSizePerWorker() {
+    return formerDsguDataSizePerWorker_;
+  }
 
   const std::vector<size_t>& getDsguDataSizePerWorker() {
     return dsguDataSizePerWorker_;

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessGroupSignals.hpp
@@ -71,6 +71,8 @@ const SignalType DO_DIAGNOSTICS = 37;
 const SignalType WRITE_DSG_MINMAX_COEFFICIENTS = 38;
 const SignalType WRITE_INTERPOLATED_VALUES_PER_GRID = 39;
 
+const SignalType REDUCE_SUBSPACE_SIZES_TL_AND_ALLOCATE_EXTRA_SG = 40;
+
 typedef int NormalizationType;
 const NormalizationType NO_NORMALIZATION = 0;
 const NormalizationType L1_NORMALIZATION = 1;

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessGroupWorker.hpp
@@ -116,13 +116,17 @@ class ProcessGroupWorker {
   void waitForThirdLevelCombiResult();
 
   /** computes a max reduce on the dsg's subspace sizes with the other systems */
-  void reduceSubspaceSizesThirdLevel();
+  void reduceSubspaceSizesThirdLevel(bool thirdLevelExtraSparseGrid);
 
   /** receives reduced sizes from tl pgroup and updates the dsgs */
   void waitForThirdLevelSizeUpdate();
 
   std::vector<std::unique_ptr<DistributedSparseGridUniform<CombiDataType>>> & getCombinedUniDSGVector(){
     return combinedUniDSGVector_;
+  }
+
+  std::vector<std::unique_ptr<DistributedSparseGridUniform<CombiDataType>>> & getExtraUniDSGVector(){
+    return extraUniDSGVector_;
   }
 
   TaskContainer& getTasks(){
@@ -147,6 +151,11 @@ class ProcessGroupWorker {
    * Vector containing all distributed sparse grids
    */
   std::vector<std::unique_ptr<DistributedSparseGridUniform<CombiDataType>>> combinedUniDSGVector_;
+
+  /**
+   * Vector containing the third level extra distributed sparse grids
+   */
+  std::vector<std::unique_ptr<DistributedSparseGridUniform<CombiDataType>>> extraUniDSGVector_;
 
   CombiParameters combiParameters_;
 

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessManager.hpp
@@ -77,7 +77,7 @@ class ProcessManager {
 
   inline size_t pretendCombineThirdLevel(std::vector<long long> numDofsToCommunicate, bool checkValues);
 
-  inline void unifySubspaceSizesThirdLevel();
+  inline void unifySubspaceSizesThirdLevel(bool thirdLevelExtraSparseGrid);
 
   void monteCarloThirdLevel(size_t numPoints, std::vector<std::vector<real>>& coordinates, std::vector<CombiDataType>& values);
 
@@ -424,11 +424,13 @@ size_t ProcessManager::pretendCombineThirdLevel(std::vector<long long> numDofsTo
  * sizes back to the workers of the third level pg who will then distribute it
  * to the other pgs.
  */
-void ProcessManager::unifySubspaceSizesThirdLevel() {
-  // tell other pgroups to idle and wait for update
-  for (auto& pg : pgroups_) {
-    if (pg != thirdLevelPGroup_)
-      pg->waitForThirdLevelSizeUpdate();
+void ProcessManager::unifySubspaceSizesThirdLevel(bool thirdLevelExtraSparseGrid) {
+  if (!thirdLevelExtraSparseGrid) {
+    // tell other pgroups to idle and wait for update
+    for (auto& pg : pgroups_) {
+      if (pg != thirdLevelPGroup_)
+        pg->waitForThirdLevelSizeUpdate();
+    }
   }
 
   // obtain instructions from third level manager
@@ -437,18 +439,27 @@ void ProcessManager::unifySubspaceSizesThirdLevel() {
 
   // exchange sizes with remote
   if (instruction == "send_first") {
-    thirdLevelPGroup_->reduceLocalAndRemoteSubspaceSizes(thirdLevel_, params_, true);
+    thirdLevelPGroup_->reduceLocalAndRemoteSubspaceSizes(thirdLevel_, params_, true, thirdLevelExtraSparseGrid);
   } else if (instruction == "recv_first") {
-    thirdLevelPGroup_->reduceLocalAndRemoteSubspaceSizes(thirdLevel_, params_, false);
+    thirdLevelPGroup_->reduceLocalAndRemoteSubspaceSizes(thirdLevel_, params_, false, thirdLevelExtraSparseGrid);
   }
   thirdLevel_.signalReady();
 
   waitAllFinished();
 
+  const auto& formerDsguDataSizePerWorker = thirdLevelPGroup_->getFormerDsguDataSizePerWorker();
   const auto& dsguDataSizePerWorker = thirdLevelPGroup_->getDsguDataSizePerWorker();
 
+  auto formerDsguDataSize = std::accumulate(formerDsguDataSizePerWorker.begin(), formerDsguDataSizePerWorker.end(), 0);
   auto dsguDataSize = std::accumulate(dsguDataSizePerWorker.begin(), dsguDataSizePerWorker.end(), 0);
+  Stats::setAttribute("formerDsguDataSize", std::to_string(formerDsguDataSize));
   Stats::setAttribute("dsguDataSize", std::to_string(dsguDataSize));
+
+  if (thirdLevelExtraSparseGrid && dsguDataSize > formerDsguDataSize) {
+    throw std::runtime_error("why is it larger???");
+  } else if (!thirdLevelExtraSparseGrid && dsguDataSize < formerDsguDataSize) {
+    throw std::runtime_error("why is it smaller???");
+  }
 }
 
 /** This function performs the so-called recombination. First, the combination

distributedcombigrid/src/sgpp/distributedcombigrid/sparsegrid/DistributedSparseGridUniform.hpp
@@ -82,6 +82,9 @@ class DistributedSparseGridUniform {
   // creates data if necessary and sets all data elements to zero
   void setZero();
 
+  // return all level vectors
+  inline const std::vector<LevelVector>& getAllLevelVectors() const;
+
   // return level vector of subspace i
   inline const LevelVector& getLevelVector(size_t i) const;
 
@@ -401,6 +404,12 @@ std::vector<LevelVector> DistributedSparseGridUniform<FG_ELEMENT>::createLevels(
 //     subspaces_[i].size_ = size_t(tmp);
 //   }
 // }
+
+template <typename FG_ELEMENT>
+inline const std::vector<LevelVector>&
+DistributedSparseGridUniform<FG_ELEMENT>::getAllLevelVectors() const {
+  return levels_;
+}
 
 template <typename FG_ELEMENT>
 inline const LevelVector& DistributedSparseGridUniform<FG_ELEMENT>::getLevelVector(size_t i) const {

commit b15551a02c9cc8b987a9af95a4c2dbd3523aca6b

worker: fix assert introduced in 566b85a461c94fd

distributedcombigrid/src/sgpp/distributedcombigrid/manager/ProcessGroupWorker.cpp
@@ -1217,7 +1217,7 @@ void ProcessGroupWorker::combineThirdLevel() {
       // copy partial data from uniDSG to extraDSG
       for (size_t i = 0; i < uniDsg->getNumSubspaces(); ++i) {
         assert(dsgToUse->getDataSize(i) == 0 ||
-               dsgToUse->getDataSize(i) == uniDsg->getDataSize()[i]);
+               dsgToUse->getDataSize(i) == uniDsg->getDataSize(i));
         std::copy_n(uniDsg->getData(i), dsgToUse->getDataSize(i), dsgToUse->getData(i));
       }
     }