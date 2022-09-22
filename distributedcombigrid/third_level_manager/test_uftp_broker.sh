4f9b2b5a7f4f5575cd3705c9311496bd56f8e364
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2022-09-22 13:52:09 +0200

    test_uftp_broker: more structure

--- distributedcombigrid/third_level_manager/test_uftp_broker.sh
+++ distributedcombigrid/third_level_manager/test_uftp_broker.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# execute this script on skx-arch.supermuc.lrz.de
+# as described on https://doku.lrz.de/display/PUBLIC/Data+Transfer+Options+on+SuperMUC-NG#DataTransferOptionsonSuperMUCNG-UNICOREFileTransfer(UFTP)
 
-FILELRZ=/hppfs/work/pn34mi/di39qun2/DisCoTec-third-level/distributedcombigrid/third_level_manager/dsg_part_0_0
-FILEHAWK=/lustre/hpe/ws10/ws10.3/ws/TODO/dsg_part_1_0
+module load uftp-client
+
+PATHLRZ=/hppfs/work/pn34mi/di39qun2/DisCoTec-third-level/distributedcombigrid/third_level_manager/
+PATHHAWK=/lustre/hpe/ws10/ws10.1/ws/ipvpolli-widely/DisCoTec-third-level/distributedcombigrid/third_level_manager/
+FILELRZ=$PATHLRZ/dsg_part_0_0
+FILEHAWK=$PATHHAWK/dsg_part_1_0
 #TODO test
-HAWKURL=gridftp-fr1.hww.hlrs.de:9000
+HAWKURL=gridftp-fr1.hww.hlrs.de:9000/rest/auth/HLRS
 
 # start with one iteration of copying file back and forth
 while true
@@ -11,16 +17,17 @@ do
 if test -f "$FILELRZ" then
     # ./copy_sng_to_hawk.sh
     # try to copy the file
-    uftp cp $FILELRZ $FILEHAWK
+    uftp cp  -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $FILELRZ $HAWKURL:$PATHHAWK
+    break
 fi
 done
 
 while true
+do
 if test -f "$FILEHAWK" then
     # ./copy_hawk_to_sng.sh
     # try to copy the file
-    uftp cp $HAWKURL/$FILEHAWK $FILELRZ
+    uftp cp  -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $HAWKURL:$FILEHAWK $PATHLRZ/
 fi
 sleep 20s
 done
-
