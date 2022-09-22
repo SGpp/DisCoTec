642577f72f59da7d9d480094e468be190bb9bb92
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2022-09-22 15:00:42 +0200

    updated broker to widely exchange files

--- distributedcombigrid/third_level_manager/test_uftp_broker.sh
+++ distributedcombigrid/third_level_manager/test_uftp_broker.sh
@@ -4,30 +4,55 @@
 
 module load uftp-client
 
-PATHLRZ=/hppfs/work/pn34mi/di39qun2/DisCoTec-third-level/distributedcombigrid/third_level_manager/
-PATHHAWK=/lustre/hpe/ws10/ws10.1/ws/ipvpolli-widely/DisCoTec-third-level/distributedcombigrid/third_level_manager/
-FILELRZ=$PATHLRZ/dsg_part_0_0
-FILEHAWK=$PATHHAWK/dsg_part_1_0
-#TODO test
-HAWKURL=gridftp-fr1.hww.hlrs.de:9000/rest/auth/HLRS
+PATHLRZ=//hppfs/scratch/02/di39qun2/third_level_manager/
+PATHHAWK=//lustre/hpe/ws10/ws10.1/ws/ipvpolli-widely/DisCoTec-third-level/distributedcombigrid/third_level_manager/
+FILELRZ=dsg_part_0_*
+FILEHAWK=dsg_part_1_*
+HAWKURL=https://gridftp-fr1.hww.hlrs.de:9000/rest/auth/HLRS
+USERHAWK=ipvpolli
 
-# start with one iteration of copying file back and forth
+TOKEN_TRANSFER_FORWARD=uftp_transfer_0.txt
+TOKEN_TRANSFER_BACKWARD=uftp_transfer_1.txt
+TOKEN_STOP=uftp_transfer_stop.txt
+
+echo "$FILELRZ" "$FILEHAWK"
+
+# loop that is infinite until $TOKEN_STOP is created
 while true
 do
-if test -f "$FILELRZ" then
-    # ./copy_sng_to_hawk.sh
-    # try to copy the file
-    uftp cp  -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $FILELRZ $HAWKURL:$PATHHAWK
-    break
-fi
-done
-
-while true
-do
-if test -f "$FILEHAWK" then
-    # ./copy_hawk_to_sng.sh
-    # try to copy the file
-    uftp cp  -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $HAWKURL:$FILEHAWK $PATHLRZ/
-fi
-sleep 20s
+    # one iteration of copying file back and forth
+    while true
+    do
+    if test -f "$PATHLRZ/$TOKEN_TRANSFER_FORWARD"; then
+        # ./copy_sng_to_hawk.sh
+        # try to copy the file
+        uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $PATHLRZ/$FILELRZ $HAWKURL:$PATHHAWK
+        rm "$PATHLRZ/$TOKEN_TRANSFER_FORWARD"
+        echo copied "$FILELRZ"
+        break
+    fi
+    if test -f "$PATHLRZ/$TOKEN_STOP"; then
+        break 2
+    fi
+    done
+    
+    while true
+    do
+    # test if the file is there by trying to copy it (maybe there is a better way?)
+    # the part after `>` is to ignore the potential (error) output
+    uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK "$HAWKURL:$PATHHAWK/$TOKEN_TRANSFER_BACKWARD" /dev/null > /dev/null 2>&1
+    cp_status=$?
+    if (exit $cp_status); then
+        # ./copy_hawk_to_sng.sh
+        # try to copy the file
+        uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $HAWKURL:$PATHHAWK/$FILEHAWK $PATHLRZ
+        uftp rm --quiet -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK "$HAWKURL:$PATHHAWK/$TOKEN_TRANSFER_BACKWARD"
+        echo copied "$FILEHAWK"
+        break
+    fi
+    if test -f "$PATHLRZ/$TOKEN_STOP"; then
+        break 2
+    fi
+    #sleep 20s
+    done
 done
