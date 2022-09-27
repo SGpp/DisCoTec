4dfdba9c31691438f45ece3a2b2e56c44ec80fdb
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2022-09-27 13:28:25 +0200

    uftp speed test: try different numbers of FTP/TCP streams

--- distributedcombigrid/third_level_manager/speed_test_uftp_broker.sh
+++ distributedcombigrid/third_level_manager/speed_test_uftp_broker.sh
@@ -15,58 +15,66 @@ TOKEN_TRANSFER_FORWARD=uftp_transfer_0.txt
 TOKEN_TRANSFER_BACKWARD=uftp_transfer_1.txt
 TOKEN_STOP=uftp_transfer_stop.txt
 
-NUM_STREAMS=(2 3 5 7 10 20 30 50 70 100)
+NUM_STREAMS=(100 70 50 30 20 10 7 5 3 2)
+NUM_TCP_STREAMS=(1 2 4 8 16)
 
 echo "$FILELRZ" "$FILEHAWK"
 
 # loop that is infinite until $TOKEN_STOP is created
 while true
 do
-for t in ${NUM_STREAMS[@]} ; do
-  echo num uftp streams : "$t"
-  for i in {1..10}
-  do
-    # one iteration of copying file back and forth
-    while true
-    do
-    if test -f "$PATHLRZ/$TOKEN_TRANSFER_FORWARD"; then
-        # ./copy_sng_to_hawk.sh
-        # try to copy the file
-        starttime=`date +%s`
-        uftp cp -t "$t" -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $PATHLRZ/$FILELRZ $HAWKURL:$PATHHAWK
-        uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $PATHLRZ/$TOKEN_TRANSFER_FORWARD $HAWKURL:$PATHHAWK
-        #rm "$PATHLRZ/$TOKEN_TRANSFER_FORWARD"
-        endtime=`date +%s`
-        echo copied "$FILELRZ" in `expr $endtime - $starttime` seconds.
-        break
-    fi
-    if test -f "$PATHLRZ/$TOKEN_STOP"; then
-        break 4
-    fi
-    done
-    
-    while true
-    do
-    # test if the file is there by trying to copy it (maybe there is a better way?)
-    # the part after `>` is to ignore the potential (error) output
-    uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK "$HAWKURL:$PATHHAWK/$TOKEN_TRANSFER_BACKWARD" /dev/null > /dev/null 2>&1
-    cp_status=$?
-    if (exit $cp_status); then
-        # ./copy_hawk_to_sng.sh
-        # try to copy the file
-        starttime=`date +%s`
-        uftp cp -t "$t" -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $HAWKURL:$PATHHAWK/$FILEHAWK $PATHLRZ
-        uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $HAWKURL:$PATHHAWK/$TOKEN_TRANSFER_BACKWARD $PATHLRZ
-        #uftp rm --quiet -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK "$HAWKURL:$PATHHAWK/$TOKEN_TRANSFER_BACKWARD"
-        endtime=`date +%s`
-        echo copied "$FILEHAWK" in `expr $endtime - $starttime` seconds.
-        break
-    fi
-    if test -f "$PATHLRZ/$TOKEN_STOP"; then
-        break 4
-    fi
-    #sleep 20s
+  for t in ${NUM_STREAMS[@]} ; do
+    echo num ftp streams : "$t"
+    for n in ${NUM_TCP_STREAMS[@]} ; do
+      echo num tcp streams : "$n"
+      #for i in {1..10}
+      #do
+        # one iteration of copying file back and forth
+        while true
+        do
+        if test -f "$PATHLRZ/$TOKEN_TRANSFER_FORWARD"; then
+            # ./copy_sng_to_hawk.sh
+            # try to copy the file
+            starttime=`date +%s`
+            uftp cp -t "$t" -n "$n" -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $PATHLRZ/$FILELRZ $HAWKURL:$PATHHAWK
+            uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $PATHLRZ/$TOKEN_TRANSFER_FORWARD $HAWKURL:$PATHHAWK
+            #rm "$PATHLRZ/$TOKEN_TRANSFER_FORWARD"
+            endtime=`date +%s`
+            ng_outfile=broker_ng_out_t${t}_n${n}.csv
+            echo `expr $endtime - $starttime` \n >> $ng_outfile
+            echo copied "$FILELRZ" in `expr $endtime - $starttime` seconds.
+            break
+        fi
+        if test -f "$PATHLRZ/$TOKEN_STOP"; then
+            break 5
+        fi
+        done
+        
+        while true
+        do
+        # test if the file is there by trying to copy it (maybe there is a better way?)
+        # the part after `>` is to ignore the potential (error) output
+        uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK "$HAWKURL:$PATHHAWK/$TOKEN_TRANSFER_BACKWARD" /dev/null > /dev/null 2>&1
+        cp_status=$?
+        if (exit $cp_status); then
+            # ./copy_hawk_to_sng.sh
+            # try to copy the file
+            starttime=`date +%s`
+            uftp cp -t "$t" -n "$n" -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $HAWKURL:$PATHHAWK/$FILEHAWK $PATHLRZ
+            uftp cp -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK $HAWKURL:$PATHHAWK/$TOKEN_TRANSFER_BACKWARD $PATHLRZ
+            #uftp rm --quiet -i ~/.uftp/id_uftp_to_hlrs -u $USERHAWK "$HAWKURL:$PATHHAWK/$TOKEN_TRANSFER_BACKWARD"
+            endtime=`date +%s`
+            hawk_outfile=broker_hawk_out_t${t}_n${n}.csv
+            echo `expr $endtime - $starttime` \n >> $hawk_outfile
+            echo copied "$FILEHAWK" in `expr $endtime - $starttime` seconds.
+            break
+        fi
+        if test -f "$PATHLRZ/$TOKEN_STOP"; then
+            break 5
+        fi
+        #sleep 20s
+        done
+      #done
     done
   done
-done
 done
