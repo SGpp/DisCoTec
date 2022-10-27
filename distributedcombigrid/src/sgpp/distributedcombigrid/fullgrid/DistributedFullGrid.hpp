ed18ca3c877388507a6a3f00ff6084dce525e44d
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2022-10-27 10:05:11 +0200

    registerUniformSG: reserve vector size
    (cherry picked from commit e6463280eb49599e1a0e4c6bcdc84cb2e8e03144)

--- distributedcombigrid/src/sgpp/distributedcombigrid/fullgrid/DistributedFullGrid.hpp
+++ distributedcombigrid/src/sgpp/distributedcombigrid/fullgrid/DistributedFullGrid.hpp
@@ -1058,11 +1058,11 @@ class DistributedFullGrid {
   void registerUniformSG(DistributedSparseGridUniform<FG_ELEMENT>& dsg) {
     dsg_ = &dsg;
     assert(dsg_->getDim() == dim_);
-
-    subspaceIndexToFGIndices_.clear();
-
     // all the hierarchical subspaces contained in this full grid
     const auto downwardClosedSet = combigrid::getDownSet(levels_);
+
+    subspaceIndexToFGIndices_.clear();
+    subspaceIndexToFGIndices_.reserve(downwardClosedSet.size());
 
     IndexType index = 0;
     // resize all common subspaces in dsg, if necessary
@@ -1085,6 +1085,8 @@ class DistributedFullGrid {
         }
       }
     }
+    subspaceIndexToFGIndices_.shrink_to_fit();
+    assert(subspaceIndexToFGIndices_.size() > 0);
     // if localFGIndexToLocalSGPointerList_ was already initialized,
     // it is invalidated now
     localFGIndexToLocalSGPointerList_.clear();
