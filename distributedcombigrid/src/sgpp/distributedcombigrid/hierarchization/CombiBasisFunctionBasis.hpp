9bd90f867bf648df77028178439fcdd7749c90de
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2022-12-16 09:57:51 +0100

    Basis functions: remove comments

--- distributedcombigrid/src/sgpp/distributedcombigrid/hierarchization/CombiBasisFunctionBasis.hpp
+++ distributedcombigrid/src/sgpp/distributedcombigrid/hierarchization/CombiBasisFunctionBasis.hpp
@@ -5,14 +5,6 @@
 
 namespace combigrid {
 
-/** The basis function for 1D Cell, the generalization for ND is simply
- * the tensor product. This class contains two methods since in a 1D cell
- * we have two points at the end of the cell, and the two methods returns
- * the component of the first point and the second returns the component
- * of the second point to the evaluation point <br>
- * All the evaluations should be done on the reference cell [0,1], except
- * the extrapolation, that can be [-1,2]. */
-
 class BasisFunctionBasis {
  public:
   /** empty Ctror */
