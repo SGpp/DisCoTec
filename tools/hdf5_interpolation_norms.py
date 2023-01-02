4d9bbd15858cebc9f6002e290de448b2b5c62355
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2023-01-02 14:48:52 +0100

    hdf5 interpolation norms: compare to analytical
    (cherry picked from commit be81628814919508d89ba3d206394159814cfec4)

--- tools/hdf5_interpolation_norms.py
+++ tools/hdf5_interpolation_norms.py
@@ -3,9 +3,37 @@
 """Script for evaluating errors and norms from hdf5 output (like generated from writeInterpolatedValues, writeInterpolationCoordinates) """
 
 import argparse
+import math
 import h5py
 from icecream import ic
 import numpy as np
+
+
+def paraboloid_function(coords):
+    dim = len(coords)
+    sign = 1. if dim % 2 == 1 else -1.
+    result = sign
+    for coord in coords:
+        result *= coord * (coord - 1.)
+    return result
+
+
+def gaussian_function(coords, time):
+    exponent = 0.
+    sigma = 1./3.
+    sigma_squared = sigma * sigma
+    sigma_squared_inv = 1. / sigma_squared
+    for dim in range(len(coords)):
+        # transform coordinates acccording to periodic boundary condition
+        coords[dim] = math.fmod(1.0 + math.fmod(coords[dim] - time, 1.0), 1.0)
+        assert(coords[dim] >= 0.)
+        assert(coords[dim] <= 1.)
+        exponent -= pow(coords[dim] - 0.5, 2)
+
+    exponent *= sigma_squared_inv
+    # leave out normalization, such that maximum is always 1
+    return np.exp(exponent)  # / sqrt( pow(2*pi*sigma_squared, len(coords)))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -48,14 +76,15 @@ if __name__ == "__main__":
     ic(values_dataset)
     assert (isinstance(values_dataset, h5py.Dataset))
     values_np = np.array(values_dataset)
+    num_samples = values_np.shape[0]
 
     # for this dataset, also get the time attribute
     assert("simulation_time" in values_dataset.attrs)
     simulation_time = values_dataset.attrs["simulation_time"]
     ic(simulation_time)
 
-    # the interpolation coordinates are only required if we compute error norms
     if calculate_error_norms:
+        # the interpolation coordinates are only required if we compute error norms
         assert args.coordinates is not None
         coordinates_h5 = h5py.File(args.coordinates[0], 'r')
         assert (len(coordinates_h5.keys()) == 1)
@@ -67,13 +96,30 @@ if __name__ == "__main__":
         ic(coordinates_dataset)
         assert (isinstance(coordinates_dataset, h5py.Dataset))
         assert values_dataset.shape[0] == coordinates_dataset.shape[0]
+        number_dimensions = coordinates_dataset.shape[1]
         coordinates_np = np.array(coordinates_dataset)
 
-        raise NotImplementedError("Error norms not implemented yet")
+        if reference_solution[0] == 'advection':
+            reference_values_np = np.array(
+                [gaussian_function(coords, simulation_time) for coords in coordinates_np])
+        elif reference_solution[0] == 'paraboloid':
+            reference_values_np = np.array(
+                [paraboloid_function(coords) for coords in coordinates_np])
+        elif reference_solution[0] == 'hyperplane':
+            raise NotImplementedError("Analytical solution not implemented yet for hyperplane")
+
+        error_np = values_np - reference_values_np
+        for p in [np.inf, 1, 2]:
+            p_error_norm = np.linalg.norm(error_np, ord=p)
+            if p in [1, 2]:
+                p_error_norm = p_error_norm / num_samples
+            print("Monte Carlo error integral of order " + str(p) + " : " + str(p_error_norm))
 
     else:
         # otherwise, we just compute the Monte Carlo norms of the interpolated values
         # (assuming domain to be an arbitrary hypercube of volume 1)
         for p in [np.inf, 1, 2]:
             p_norm = np.linalg.norm(values_np, ord=p)
-            print("Monte Carlo norm of order "+ str(p) +" : " + str(p_norm))
+            if p in [1, 2]:
+                p_norm = p_norm / num_samples
+            print("Monte Carlo norm of order " + str(p) + " : " + str(p_norm))
