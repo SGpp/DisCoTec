168732f89858a5a899ae95280a50dd27072cfced
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2023-02-14 16:07:15 +0100

    test third level: longer timeout

--- tests/test_thirdLevel.cpp
+++ tests/test_thirdLevel.cpp
@@ -817,7 +817,7 @@ void testPretendThirdLevel(TestParams& testParams) {
   BOOST_CHECK(!TestHelper::testStrayMessages(testParams.comm));
 }
 
-BOOST_FIXTURE_TEST_SUITE(thirdLevel, TestHelper::BarrierAtEnd, *boost::unit_test::timeout(600))
+BOOST_FIXTURE_TEST_SUITE(thirdLevel, TestHelper::BarrierAtEnd, *boost::unit_test::timeout(1000))
 
 BOOST_AUTO_TEST_CASE(test_0, *boost::unit_test::tolerance(TestHelper::tolerance)) {
   unsigned int numSystems = 2;
