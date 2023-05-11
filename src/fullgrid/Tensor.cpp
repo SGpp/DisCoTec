2471de38af4fffb3316d03ad62275f6bae295b0a
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2023-05-11 09:44:42 +0200

    Tensor: explicit instantiations

---
+++ src/fullgrid/Tensor.cpp
@@ -0,0 +1,32 @@
+#include "fullgrid/Tensor.hpp"
+#include "utils/Config.hpp"
+
+// #include <boost/preprocessor.hpp> //TODO consider using this for automatic instantiations
+
+namespace combigrid {
+
+template class TensorIndexer<2>;
+template class TensorIndexer<3>;
+template class TensorIndexer<4>;
+template class TensorIndexer<5>;
+template class TensorIndexer<6>;
+
+template class Tensor<float, 2>;
+template class Tensor<float, 3>;
+template class Tensor<float, 4>;
+template class Tensor<float, 5>;
+template class Tensor<float, 6>;
+
+template class Tensor<double, 2>;
+template class Tensor<double, 3>;
+template class Tensor<double, 4>;
+template class Tensor<double, 5>;
+template class Tensor<double, 6>;
+
+template class Tensor<combigrid::complex, 2>;
+template class Tensor<combigrid::complex, 3>;
+template class Tensor<combigrid::complex, 4>;
+template class Tensor<combigrid::complex, 5>;
+template class Tensor<combigrid::complex, 6>;
+
+}  // namespace
