0f00d8e492ad5a27d2fa53c6648fee321898f7c3
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2023-05-17 09:37:39 +0200

    Stats: use collective buffering for json output

--- src/io/MPIInputOutput.hpp
+++ src/io/MPIInputOutput.hpp
@@ -26,11 +26,6 @@ static MPI_Info getNewConsecutiveMpiInfo() {
   // do not use this -- force-enables collective buffering!!
   // //  MPI_Info_set(info, "romio_no_indep_rw", "true");
 
-  // // disable caching of file contents in kernel
-  // MPI_Info_set(info, "direct_io", "true");
-  // MPI_Info_set(info, "direct_read", "true");
-  // MPI_Info_set(info, "direct_write", "true");
-  
   // disable ROMIO's data-sieving
   MPI_Info_set(info, "romio_ds_read", "disable");
   MPI_Info_set(info, "romio_ds_write", "disable");
@@ -39,29 +34,36 @@ static MPI_Info getNewConsecutiveMpiInfo() {
 
 template <typename T>
 bool writeValuesConsecutive(const T* valuesStart, MPI_Offset numValues, const std::string& fileName,
-                            combigrid::CommunicatorType comm, bool replaceExistingFile = false) {
+                            combigrid::CommunicatorType comm, bool replaceExistingFile = false,
+                            bool withCollectiveBuffering = false) {
   // get offset in file
   MPI_Offset pos = 0;
   MPI_Exscan(&numValues, &pos, 1, MPI_OFFSET, MPI_SUM, comm);
 
   MPI_Info info = getNewConsecutiveMpiInfo();
   MPI_Info_set(info, "access_style", "write_once");
-  int mpi_size;
-  MPI_Comm_size(comm, &mpi_size);
-  MPI_Info_set(info, "nb_proc", std::to_string(mpi_size).c_str());
-  // disable ROMIO's collective buffering //TODO test
-  MPI_Info_set(info, "romio_cb_write", "disable");
-  MPI_Info_set(info, "romio_cb_read", "disable");
+  if (withCollectiveBuffering) {
+    // enable ROMIO's collective buffering
+    MPI_Info_set(info, "romio_no_indep_rw", "enable");
+    MPI_Info_set(info, "romio_cb_write", "enable");
+    MPI_Info_set(info, "romio_cb_read", "enable");
+  } else {
+    // disable ROMIO's collective buffering //TODO test
+    MPI_Info_set(info, "romio_cb_write", "disable");
+    MPI_Info_set(info, "romio_cb_read", "disable");
+  }
 
   // open file
   MPI_File fh;
   int err = MPI_File_open(comm, fileName.c_str(), MPI_MODE_CREATE | MPI_MODE_EXCL | MPI_MODE_WRONLY,
                           info, &fh);
-  if (err != MPI_SUCCESS) { 
+  if (err != MPI_SUCCESS) {
     auto openErrorString = getMpiErrorString(err);
     if (err != MPI_ERR_FILE_EXISTS && openErrorString.find("File exists") == std::string::npos) {
-       // there are some weird compilers that define a new error code for "file exists"...
-       std::cerr << "potential write/open error: " << std::to_string(err) << " " << openErrorString << std::endl;
+      // there are some weird MPI-IO implementations that define a new error code for "file
+      // exists"...
+      std::cerr << "potential write/open error: " << std::to_string(err) << " " << openErrorString
+                << std::endl;
     }
     if (replaceExistingFile) {
       // file already existed, delete it and create new file
@@ -79,8 +81,8 @@ bool writeValuesConsecutive(const T* valuesStart, MPI_Offset numValues, const st
     }
   }
   if (err != MPI_SUCCESS) {
-    std::cerr << "Open error " << fileName << " :" << std::to_string(err) << " " << getMpiErrorString(err) << std::endl;
-    // throw std::runtime_error("MPI file open error: " + getMpiErrorString(err));
+    std::cerr << "Open error " << fileName << " :" << std::to_string(err) << " "
+              << getMpiErrorString(err) << std::endl;
   }
 
   // write to single file with MPI-IO
@@ -170,7 +172,8 @@ bool readValuesConsecutive(T* valuesStart, MPI_Offset numValues, const std::stri
 template <typename T, typename ReduceFunctionType>
 bool readReduceValuesConsecutive(T* valuesStart, MPI_Offset numValues, const std::string& fileName,
                                  combigrid::CommunicatorType comm, int numElementsToBuffer,
-                                 ReduceFunctionType reduceFunction, bool withCollectiveBuffering = false) {
+                                 ReduceFunctionType reduceFunction,
+                                 bool withCollectiveBuffering = false) {
   MPI_Offset pos = 0;
   MPI_Exscan(&numValues, &pos, 1, MPI_OFFSET, MPI_SUM, comm);
   MPI_Info info = getNewConsecutiveMpiInfo();
