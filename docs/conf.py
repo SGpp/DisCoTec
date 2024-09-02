25459344b1540bc2f5f3faf9679752d113412fa9
Theresa Pollinger <theresa.pollinger@riken.jp>
2024-09-02 17:11:24 +0900

    docs: add basic sphinx configuration (generates with  doxygen Doxyfile && make html )

---
+++ docs/conf.py
@@ -0,0 +1,58 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DisCoTec'
+copyright = '2024, The SGpp Team'
+author = 'The SGpp Team'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'breathe',
+    'exhale',
+    'sphinx_rtd_theme',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+# -- Exhale configuration ---------------------------------------------------
+# Setup the breathe extension
+breathe_projects = {
+    "DisCoTec": "./generated_docs/xml"
+}
+breathe_default_project = "DisCoTec"
+
+ # Setup the exhale extension
+exhale_args = {
+    # These arguments are required
+    "containmentFolder":     "./api",
+    "rootFileName":          "library_root.rst",
+    "rootFileTitle":         "Library API",
+    "doxygenStripFromPath":  "..",
+    # Suggested optional arguments
+    "createTreeView":        True,
+    # TIP: if using the sphinx-bootstrap-theme, you need
+    # "treeViewIsBootstrap": True,
+}
+
+# Tell sphinx what the primary language being documented is.
+primary_domain = 'cpp'
+
+# Tell sphinx what the pygments highlight language should be.
+highlight_language = 'cpp'
---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. DisCoTec documentation master file, created by
+   sphinx-quickstart on Thu Aug 29 13:22:11 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+DisCoTec's documentation!
+====================================
+
+This is about the C++ code for HPC simulations with the sparse grid combination technique.
+Not to be confused with the `International Federated Conference on Distributed Computing Techniques <http://www.discotec.org/>`!
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+   
+   api/library_root
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
